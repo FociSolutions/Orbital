d6e9fb897fc88fa1491eca613c331f8934af48b9
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-07-16 13:52:42 -0400

    Added tests for MockDefinition

--- tests/Orbital.Mock.Server.Tests/Models/MockDefinitionTests.cs
+++ tests/Orbital.Mock.Server.Tests/Models/MockDefinitionTests.cs
@@ -1,10 +1,164 @@
-﻿using System;
+﻿using Bogus;
+using Orbital.Mock.Server.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace Orbital.Mock.Server.Tests.Models
 {
-    class MockDefinitionTests
+    public class MockDefinitionTests
     {
+        [Fact]
+        public void EqualsObjectSuccessTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+            .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+            .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+            .RuleFor(m => m.Host, f => f.Internet.DomainName())
+            .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+
+            var mockDefinitionInfo = mockDefinitionFake.Generate();
+
+            var input = new
+            {
+                objTest = mockDefinitionInfo as Object
+            };
+
+            #endregion
+
+            var Actual = mockDefinitionInfo.Equals(input.objTest);
+            Assert.True(Actual);
+        }
+
+        [Fact]
+        public void EqualsObjectFailsTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+            .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+            .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+            .RuleFor(m => m.Host, f => f.Internet.DomainName())
+            .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+            var mockDefinitionInfo = mockDefinitionFake.Generate();
+
+            var input = new
+            {
+                objTest = new MockDefinition { Host = mockDefinitionInfo.Host, Metadata = null } as Object
+            };
+
+            #endregion
+
+            var Actual = mockDefinitionInfo.Equals(input.objTest);
+            Assert.False(Actual);
+        }
+
+        [Fact]
+        public void EqualsSuccessTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+            .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+            .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+            .RuleFor(m => m.Host, f => f.Internet.DomainName())
+            .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+
+            var mockDefinitionInfo = mockDefinitionFake.Generate();
+
+            var input = new
+            {
+                mockDefinitionTest = mockDefinitionInfo
+            };
+
+            #endregion
+
+            var Actual = mockDefinitionInfo.Equals(input.mockDefinitionTest);
+            Assert.True(Actual);
+        }
+
+        [Fact]
+        public void EqualsFailsNullTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+            .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+            .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+            .RuleFor(m => m.Host, f => f.Internet.DomainName())
+            .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+
+            var mockDefinitionInfo = mockDefinitionFake.Generate();
+
+            #endregion
+
+            var Actual = mockDefinitionInfo.Equals(null);
+            Assert.False(Actual);
+        }
+
+        [Fact]
+        public void EqualsFailsHostTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+            .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+            .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+            .RuleFor(m => m.Host, f => f.Internet.DomainName())
+            .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+
+            var mockDefinitionInfo = mockDefinitionFake.Generate();
+
+            var input = new
+            {
+                mockDefinitionTest = new MockDefinition { Metadata = mockDefinitionInfo.Metadata }
+            };
+
+
+            #endregion
+
+            var Actual = mockDefinitionInfo.Equals(input.mockDefinitionTest);
+            Assert.False(Actual);
+        }
+
+        [Fact]
+        public void EqualsFailsMetadataInfoTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+            .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+            .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+            .RuleFor(m => m.Host, f => f.Internet.DomainName())
+            .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+
+            var mockDefinitionInfo = mockDefinitionFake.Generate();
+
+            var input = new
+            {
+                mockDefinitionTest = new MockDefinition { Host = mockDefinitionInfo.Host }
+            };
+
+
+            #endregion
+
+            var Actual = mockDefinitionInfo.Equals(input.mockDefinitionTest);
+            Assert.False(Actual);
+        }
     }
 }
