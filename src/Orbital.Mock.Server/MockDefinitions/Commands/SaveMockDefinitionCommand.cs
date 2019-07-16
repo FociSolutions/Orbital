97697999ecbc5a5796c97ee12d1dd40af088d2e6
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-07-16 15:58:11 -0400

    added comments

--- src/Orbital.Mock.Server/MockDefinitions/Commands/SaveMockDefinitionCommand.cs
+++ src/Orbital.Mock.Server/MockDefinitions/Commands/SaveMockDefinitionCommand.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 
 namespace Orbital.Mock.Server.MockDefinitions.Commands
 {
+    /// <summary>
+    /// Class SaveMockDefinitionCommand represents a request for saving a mock definition to the cache
+    /// </summary>
     public class SaveMockDefinitionCommand : IRequest
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mockDefinition">Mock definition to store in cache</param>
         public SaveMockDefinitionCommand(MockDefinition mockDefinition)
         {
             this.MockDefinition = mockDefinition;
         }
 
+        /// <summary>
+        /// Mockdefinition property
+        /// </summary>
         public MockDefinition MockDefinition { get; }
     }
 }
--- src/Orbital.Mock.Server/MockDefinitions/Handlers/SaveMockDefinitionHandler.cs
+++ src/Orbital.Mock.Server/MockDefinitions/Handlers/SaveMockDefinitionHandler.cs
@@ -9,18 +9,30 @@ using System.Threading.Tasks;
 
 namespace Orbital.Mock.Server.Handlers
 {
-
+    /// <summary>
+    /// Class SaveMockDefinitionHandler that handles incoming requests to save a mock definition
+    /// </summary>
     public class SaveMockDefinitionHandler : IRequestHandler<SaveMockDefinitionCommand>
     {
         private readonly IMemoryCache cache;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="cache">Cache to save the mock definition</param>
         public SaveMockDefinitionHandler(IMemoryCache cache)
         {
             this.cache = cache;
         }
 
+        /// <summary>
+        /// Asynchronously handles saving the mock definition to cache.
+        /// </summary>
+        /// <param name="request">SaveMockDefinition command</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public Task<Unit> Handle(SaveMockDefinitionCommand request, CancellationToken cancellationToken)
-        {            
+        {
             this.cache.Set(request.MockDefinition.Metadata.Title, request.MockDefinition);
             return Unit.Task;
         }
