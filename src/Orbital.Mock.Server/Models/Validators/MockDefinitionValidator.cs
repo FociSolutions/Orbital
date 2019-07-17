0c0f21f666b65921553472913587cd985fcc442c
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-07-17 16:07:13 -0400

    added mock definition properties and extended validation to added properties

--- src/Orbital.Mock.Server/Models/Validators/MockDefinitionValidator.cs
+++ src/Orbital.Mock.Server/Models/Validators/MockDefinitionValidator.cs
@@ -1,4 +1,6 @@
 ﻿using FluentValidation;
+using Microsoft.OpenApi.Validations;
+using Microsoft.OpenApi.Validations.Rules;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +13,25 @@ namespace Orbital.Mock.Server.Models.Validators
     /// </summary>
     public class MockDefinitionValidator : AbstractValidator<MockDefinition>
     {
+        private OpenApiValidator openApiValidator;
 
         /// <summary>
         /// 
         /// </summary>
         public MockDefinitionValidator()
         {
+            this.openApiValidator = new OpenApiValidator(new ValidationRuleSet(ValidationRuleSet.GetDefaultRuleSet()));
             RuleFor(x => x.Host).NotEmpty();
+            RuleFor(x => x.OpenApi).NotEmpty();
+            RuleFor(x => x.BasePath).NotEmpty();
+            RuleFor(x => helper(x)).Empty();
             RuleFor(x => x.Metadata).InjectValidator();
+        }
+
+        private IEnumerable<OpenApiValidatorError> helper(MockDefinition x)
+        {
+            openApiValidator.Visit(x.OpenApi);
+            return openApiValidator.Errors;
         }
     }
 }
