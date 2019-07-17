d993c081f9dae61e9870d30cbd5e80625d4306c0
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-07-17 14:23:41 -0400

    added test for delete method in controller

--- tests/Orbital.Mock.Server.Tests/Controllers/OrbitalAdminControllerTests.cs
+++ tests/Orbital.Mock.Server.Tests/Controllers/OrbitalAdminControllerTests.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using NSubstitute;
 using Orbital.Mock.Server.Controllers;
 using Orbital.Mock.Server.MockDefinitions.Commands;
@@ -108,6 +109,42 @@ namespace Orbital.Mock.Server.Tests.Controllers
 
             Assert.Equal(Actual, Expected);
 
+        }
+
+        [Fact]
+        public void DeleteSuccess()
+        {
+            #region TestSetup
+            var metadataFake = new Faker<MetadataInfo>()
+                   .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+                   .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+                .RuleFor(m => m.Host, f => f.Internet.DomainName())
+                .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+            var input = new
+            {
+                mockDefinition = mockDefinitionFake.Generate(),
+                httpContext = new DefaultHttpContext()
+            };
+
+            var mediatorMock = Substitute.For<IMediator>();
+
+            mediatorMock.Send(Arg.Any<DeleteMockDefinitionByTitleCommand>(), Arg.Any<CancellationToken>());
+
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = input.httpContext
+            };
+            #endregion
+
+            var Target = new OrbitalAdminController(mediatorMock)
+            {
+                ControllerContext = controllerContext
+            };
+
+            Assert.IsType<OkResult>(Target.Delete(input.mockDefinition.Metadata.Title));
         }
     }
 }
