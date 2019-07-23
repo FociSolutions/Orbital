6ab06c58d0f006670e8d35c5d84f9c54abcb03ed
Francisco Amezcua <francisco.amezcua@focisolutions.com>
2019-07-23 15:47:58 -0400

    Added test for admin controller for Put Updated and Get all Test.

--- tests/Orbital.Mock.Server.Tests/Controllers/OrbitalAdminControllerTests.cs
+++ tests/Orbital.Mock.Server.Tests/Controllers/OrbitalAdminControllerTests.cs
@@ -146,5 +146,140 @@ namespace Orbital.Mock.Server.Tests.Controllers
 
             Assert.IsType<OkResult>(Target.Delete(input.mockDefinition.Metadata.Title));
         }
+
+        [Fact]
+        public void GetAllSuccessTest()
+        {
+            #region Test Setup
+
+            var metadataFake = new Faker<MetadataInfo>()
+                   .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+                   .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+                .RuleFor(m => m.Host, f => f.Internet.DomainName())
+                .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+            var input = new
+            {
+                mockDefinitions = mockDefinitionFake.Generate(5),
+                httpContext = new DefaultHttpContext()
+            };
+
+            var mediatorMock = Substitute.For<IMediator>();
+
+            mediatorMock.Send(Arg.Any<GetAllMockDefinitionsCommand>(), Arg.Any<CancellationToken>()).Returns(input.mockDefinitions);
+
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = input.httpContext
+            };
+            #endregion
+
+            var Target = new OrbitalAdminController(mediatorMock)
+            {
+                ControllerContext = controllerContext
+            };
+
+            var Actual = Target.GetAll().Value;
+            var Expected = input.mockDefinitions;
+
+            Assert.Equal(Actual, Expected);
+
+        }
+
+        [Fact]
+        public void PutCreatedSuccessTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+                    .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+                    .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+                .RuleFor(m => m.Host, f => f.Internet.DomainName())
+                .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+
+            var input = new
+            {
+                mockDefinition = mockDefinitionFake.Generate(),
+                httpContext = new DefaultHttpContext(),
+                path = "/api/v1.0/orbitaladmin"
+            };
+
+            input.httpContext.Request.Path = input.path;
+
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = input.httpContext
+            };
+
+            var mediatorMock = Substitute.For<IMediator>();
+            #endregion
+
+            var Target = new OrbitalAdminController(mediatorMock)
+            {
+                ControllerContext = controllerContext
+            };
+
+            var encoded = Uri.EscapeUriString(input.mockDefinition.Metadata.Title);
+
+            var Expected = new
+            {
+                location = $"{input.path}/{encoded}",
+                value = input.mockDefinition
+            };
+
+            var Actual = Target.Put(input.mockDefinition);
+
+            Assert.IsType<CreatedResult>(Actual);
+            var actualCreateResult = Actual as CreatedResult;
+
+            Assert.Equal(Expected.location, actualCreateResult.Location);
+            Assert.Equal(Expected.value, actualCreateResult.Value);
+        }
+
+        [Fact]
+        public void PutUpdatedSuccessTest()
+        {
+            #region Test Setup
+            var metadataFake = new Faker<MetadataInfo>()
+                    .RuleFor(m => m.Title, f => f.Lorem.Sentence())
+                    .RuleFor(m => m.Description, f => f.Lorem.Paragraph());
+
+            var mockDefinitionFake = new Faker<MockDefinition>()
+                .RuleFor(m => m.Host, f => f.Internet.DomainName())
+                .RuleFor(m => m.Metadata, f => metadataFake.Generate());
+
+
+            var input = new
+            {
+                mockDefinition = mockDefinitionFake.Generate(),
+                httpContext = new DefaultHttpContext(),
+                path = "/api/v1.0/orbitaladmin"
+            };
+
+            input.httpContext.Request.Path = input.path;
+
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = input.httpContext
+            };
+
+            var mediatorMock = Substitute.For<IMediator>();
+            mediatorMock.Send(Arg.Any<UpdateMockDefinitionByTitleCommand>(), Arg.Any<CancellationToken>()).Returns<MockDefinition>(input.mockDefinition);
+            #endregion
+            var Target = new OrbitalAdminController(mediatorMock)
+            {
+                ControllerContext = controllerContext
+            };
+
+            var encoded = Uri.EscapeUriString(input.mockDefinition.Metadata.Title);
+
+            var Actual = Target.Put(input.mockDefinition);
+
+            Assert.IsType<OkResult>(Actual);
+        }
     }
 }
