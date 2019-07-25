25d06424a278e81f676f6fb71ced2256f21e9f02
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-07-25 15:11:17 -0400

    validation filter setup

---
+++ src/Orbital.Mock.Server/Pipelines/Filters/ValidationFilter.cs
@@ -0,0 +1,42 @@
+﻿using Orbital.Mock.Server.Pipelines.Filters.Bases;
+using Orbital.Mock.Server.Pipelines.Ports.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbital.Mock.Server.Pipelines.Filters
+{
+    internal class ValidationFilter<T> : FaultableBaseFilter<T>
+        where T : IFaultablePort, IValidationPort
+    {
+
+
+        public override T Process(T port)
+        {
+            if (!IsPortValid(port, out port))
+            {
+                return port;
+            }
+
+            var path = port.Path;
+            var verb = port.Verb;
+
+            if (path == "")
+            {
+                var error = "Path cannot be null";
+                return (T)port.AppendFault(new ArgumentNullException(error));
+            }
+
+            if (verb == "")
+            {
+                var error = "Verb cannot be null";
+                return (T)port.AppendFault(new ArgumentNullException(error));
+            }
+
+            return port;
+
+        }
+
+    }
+}
---
+++ src/Orbital.Mock.Server/Pipelines/Ports/Interfaces/IValidationPort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbital.Mock.Server.Pipelines.Ports.Interfaces
+{
+    internal interface IValidationPort
+    {
+        string Path { get; set; }
+
+        string Verb { get; set; }
+    }
+}
