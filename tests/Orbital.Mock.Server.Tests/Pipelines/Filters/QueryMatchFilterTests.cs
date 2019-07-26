83e6c54518f19a20ff95bba41d05543c1b9186f9
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-07-26 16:51:07 -0400

    end of day commit and push

---
+++ tests/Orbital.Mock.Server.Tests/Pipelines/Filters/QueryMatchFilterTests.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Primitives;
+using Orbital.Mock.Server.Models;
+using Orbital.Mock.Server.Pipelines.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Orbital.Mock.Server.Tests.Pipelines.Filters
+{
+    public class QueryMatchFilterTests
+    {
+        [Fact]
+        public void QueryMatchFilterSuccessTest()
+        {
+            #region TestSetup
+            var fakeString = new Faker<string>().RuleFor(x => x, f => f.Lorem.Word());
+
+            var scenarioFaker = new Faker<Scenario>()
+                .RuleFor(m => m.RequestMatchRules, f => new RequestMatchRules
+                {
+                    QueryRules = fakeString.Generate(10).ToDictionary<string, string>(val => f.Lorem.Word())
+                })
+                .RuleFor(m => m.Id, f => f.Lorem.Word());
+
+            var fakeScenario = scenarioFaker.Generate();
+
+            var input = new
+            {
+                Scenarios = new List<Scenario>() { fakeScenario },
+                Query = new QueryCollection(fakeScenario.RequestMatchRules.QueryRules.)
+            }
+            #endregion
+        }
+    }
+}
