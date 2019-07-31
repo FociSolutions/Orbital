2720d4ffeb9d5115b04806a353d07813ce431497
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-07-31 16:33:54 -0400

    forgot to change branches but don't want to redo work so saving it for now

---
+++ tests/Orbital.Mock.Server.Tests/Pipelines/MockServerProcessorTests.cs
@@ -0,0 +1,40 @@
+﻿using Orbital.Mock.Server.Pipelines;
+using Orbital.Mock.Server.Pipelines.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orbital.Mock.Server.Tests.Pipelines
+{
+    public class MockServerProcessorTests
+    {
+        [Fact]
+        public void MockServerProcessorStopAfterStart()
+        {
+            var Target = new MockServerProcessor();
+            Target.Start();
+            var Actual = Target.Stop();
+
+            Assert.True(Actual);
+        }
+
+        [Fact]
+        public void MockServerProcessorStopBeforeStart()
+        {
+            var Target = new MockServerProcessor();
+            var Actual = Target.Stop();
+            Assert.True(Actual);
+        }
+
+        [Fact]
+        public void MockServerProcessorPushWithValidInput()
+        {
+            #region TestSetup
+            #endregion
+            var Target = new MockServerProcessor();
+            Target.Start();
+
+        }
+    }
+}
