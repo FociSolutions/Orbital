ffa311ca7e1640c53d455cbbd8206ad74b645329
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-08-07 14:06:18 -0400

    resolved comments

--- src/Orbital.Mock.Server/Commands/MockDefinition/GetAllMockDefinitionsCommand.cs
+++ src/Orbital.Mock.Server/Commands/MockDefinition/GetAllMockDefinitionsCommand.cs
@@ -2,22 +2,15 @@
 using Orbital.Mock.Server.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Orbital.Mock.Server.MockDefinitions.Commands
 {
+    [ExcludeFromCodeCoverage]
     /// <summary>
     /// Class GetAllMockDefinitionsCommand represents a request for getting all mock definitions
     /// </summary>
-    public class GetAllMockDefinitionsCommand : IRequest<List<MockDefinition>>
-    {
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public GetAllMockDefinitionsCommand()
-        {
-        }
-
-    }
+    public class GetAllMockDefinitionsCommand : IRequest<List<MockDefinition>> { }
 }
