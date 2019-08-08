58035f667a6cdab8b980eac2204dbb567be8a008
Francisco Amezcua <francisco.amezcua@focisolutions.com>
2019-08-08 13:44:51 -0400

    Updated new Integration branch changes.

--- src/Orbital.Mock.Server/Pipelines/Models/Examples/MockDefinitionsModelExamples.cs
+++ src/Orbital.Mock.Server/Pipelines/Models/Examples/MockDefinitionsModelExamples.cs
@@ -7,6 +7,7 @@ using Swashbuckle.AspNetCore.Filters;
 using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace Orbital.Mock.Server.Pipelines.Models.Examples
     {
         public MockDefinition GetExamples()
         {
+            
             DateTime dateTime = new DateTime();
             IDictionary<string, string> headers1 = new Dictionary<string, string>();
             IDictionary<string, string> headers2 = new Dictionary<string, string>();
@@ -27,7 +29,14 @@ namespace Orbital.Mock.Server.Pipelines.Models.Examples
             IDictionary<string, string> queryRules1 = new Dictionary<string, string>();
             IDictionary<string, string> queryRules2 = new Dictionary<string, string>();
             IDictionary<string, string> queryRules3 = new Dictionary<string, string>();
-            string bodyRules = "Transfer-Encoding set to chunked";
+
+            ICollection<BodyRule> bodyRules1 = new List<BodyRule>();
+            ICollection<BodyRule> bodyRules2 = new List<BodyRule>();
+            ICollection<BodyRule> bodyRules3 = new List<BodyRule>();
+
+            bodyRules1.Add(new BodyRule { Rule = "Use Json Schema", Type = BodyRuleTypes.JsonSchema});
+            bodyRules2.Add(new BodyRule { Rule = "Use Json Path", Type = BodyRuleTypes.JsonPath});
+            bodyRules3.Add(new BodyRule { Rule = "Use Body Equality", Type = BodyRuleTypes.BodyEquality});
 
             headers1.Add("Date", dateTime.ToString());
             headers2.Add("Expires", "Tue, 22 Jan 2020 18:56:00 GMT");
@@ -42,26 +51,26 @@ namespace Orbital.Mock.Server.Pipelines.Models.Examples
 
             List<Scenario> scenarios = new List<Scenario>();
             scenarios.Add(new Scenario() {Id ="id01", Metadata = new MetadataInfo{Title = "Scenario 1", Description = "Test Scenario 1" },
-                Verb = "GET", Path = "/pets", Response = new MockResponse { Status = 0, Body = "Scenario 1", Headers = headers1 },
-                RequestMatchRules = new RequestMatchRules {HeaderRules = headersRule1, QueryRules = queryRules1, BodyRules = bodyRules }
+                Verb = 0, Path = "/pets", Response = new MockResponse { Status = 0, Body = "Scenario 1", Headers = headers1 },
+                RequestMatchRules = new RequestMatchRules {HeaderRules = headersRule1, QueryRules = queryRules1, BodyRules = bodyRules1 }
             });
             scenarios.Add(new Scenario()
             {
                 Id = "id02",
                 Metadata = new MetadataInfo { Title = "Scenario 2", Description = "Test Scenario 2" },
-                Verb = "GET",
+                Verb = 0,
                 Path = "/pets",
                 Response = new MockResponse { Status = 200, Body = "Testing scenario 2", Headers = headers2 },
-                RequestMatchRules = new RequestMatchRules { HeaderRules = headersRule2, QueryRules = queryRules2, BodyRules = bodyRules }
+                RequestMatchRules = new RequestMatchRules { HeaderRules = headersRule2, QueryRules = queryRules2, BodyRules = bodyRules2 }
             });
             scenarios.Add(new Scenario()
             {
                 Id = "id03",
                 Metadata = new MetadataInfo { Title = "Scenario 3", Description = "Test Scenario 3" },
-                Verb = "GET",
+                Verb = 0,
                 Path = "/pets",
                 Response = new MockResponse { Status = 200, Body = "Scenario 3", Headers = headers3 },
-                RequestMatchRules = new RequestMatchRules { HeaderRules = headersRule3, QueryRules = queryRules3, BodyRules = bodyRules }
+                RequestMatchRules = new RequestMatchRules { HeaderRules = headersRule3, QueryRules = queryRules3, BodyRules = bodyRules3 }
             });
 
 
