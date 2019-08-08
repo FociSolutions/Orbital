392789c6758b89d898b0773978cd1d4399607d29
Francisco Amezcua <francisco.amezcua@focisolutions.com>
2019-08-08 10:20:37 -0400

    Added more content to the MockDefinitionsModelExample

--- src/Orbital.Mock.Server/Pipelines/Models/Examples/MockDefinitionsModelExamples.cs
+++ src/Orbital.Mock.Server/Pipelines/Models/Examples/MockDefinitionsModelExamples.cs
@@ -20,27 +20,43 @@ namespace Orbital.Mock.Server.Pipelines.Models.Examples
 
         public MockDefinition GetExamples()
         {
+            IDictionary<string, string> headers = new Dictionary<string, string>();
+            IDictionary<string, string> headersRules = new Dictionary<string, string>();
+            IDictionary<string, string> queryRules = new Dictionary<string, string>();
+            string bodyRules = "bodyRule";
+
+            for (int i = 0; i < 3; i++)
+            {
+                headers.Add("header" + i, "string");
+                headersRules.Add("headerRule" + i, "string");
+                queryRules.Add("queryRule" + i, "string");
+            }
+
             List<Scenario> scenarios = new List<Scenario>();
             scenarios.Add(new Scenario() {Id ="id01", Metadata = new MetadataInfo{Title = "Scenario 1", Description = "Test Scenario 1" },
-                Verb = "", Path = "", Response = new MockResponse(), RequestMatchRules = new RequestMatchRules()  });
+                Verb = "GET", Path = "/pets", Response = new MockResponse { Status = 0, Body = "Scenario 1", Headers = headers },
+                RequestMatchRules = new RequestMatchRules {HeaderRules = headersRules, QueryRules = queryRules, BodyRules = bodyRules }
+            });
             scenarios.Add(new Scenario()
             {
                 Id = "id02",
                 Metadata = new MetadataInfo { Title = "Scenario 2", Description = "Test Scenario 2" },
-                Verb = "",
-                Path = "",
-                Response = new MockResponse { Status = 200, Body = "", Headers = null },
-                RequestMatchRules = new RequestMatchRules()
+                Verb = "GET",
+                Path = "/pets",
+                Response = new MockResponse { Status = 200, Body = "Testing scenario 2", Headers = headers },
+                RequestMatchRules = new RequestMatchRules { HeaderRules = headersRules, QueryRules = queryRules, BodyRules = bodyRules }
             });
             scenarios.Add(new Scenario()
             {
                 Id = "id03",
                 Metadata = new MetadataInfo { Title = "Scenario 3", Description = "Test Scenario 3" },
-                Verb = "",
-                Path = "",
-                Response = new MockResponse { Status = 200, Body = "", Headers = null },
-                RequestMatchRules = new RequestMatchRules {HeaderRules = null, BodyRules = "", QueryRules = null }
+                Verb = "GET",
+                Path = "/pets",
+                Response = new MockResponse { Status = 200, Body = "Scenario 3", Headers = headers },
+                RequestMatchRules = new RequestMatchRules { HeaderRules = headersRules, QueryRules = queryRules, BodyRules = bodyRules }
             });
+
+
             return new MockDefinition
             {
                 Host = "petstore.swagger.io",
