6eb098ce523452b3b18b75621b4a212bbcec0ae1
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-08-13 11:24:54 -0400

    fixed middleware

--- src/Orbital.Mock.Server/Middleware/LoggingRequestResponseMiddleware.cs
+++ src/Orbital.Mock.Server/Middleware/LoggingRequestResponseMiddleware.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Internal;
 using Serilog;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -47,12 +43,18 @@ namespace Orbital.Mock.Server.Middleware
 
         private async Task<string> FormatRequest(HttpRequest request)
         {
-            var body = request.Body;
-            request.EnableRewind();
-            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
-            await request.Body.ReadAsync(buffer, 0, buffer.Length);
-            var bodyAsText = Encoding.UTF8.GetString(buffer);
-            request.Body = body;
+            request.EnableBuffering();
+            string bodyAsText;
+            using (var streamReader = new StreamReader(
+                request.Body,
+                encoding: Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                bufferSize: 1024,
+                leaveOpen: true))
+            {
+                bodyAsText = streamReader.ReadToEnd();
+                request.Body.Position = 0;
+            }
             return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
         }
 
@@ -63,9 +65,5 @@ namespace Orbital.Mock.Server.Middleware
             response.Body.Seek(0, SeekOrigin.Begin);
             return $"{response.Headers} {response.StatusCode}: {text}";
         }
-
-
     }
-
 }
-
