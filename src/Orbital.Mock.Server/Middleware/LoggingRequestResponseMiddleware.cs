c79dce3142e084a0c353f2ec6435f87e537c8c16
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-08-13 10:07:08 -0400

    added logging for header on response

--- src/Orbital.Mock.Server/Middleware/LoggingRequestResponseMiddleware.cs
+++ src/Orbital.Mock.Server/Middleware/LoggingRequestResponseMiddleware.cs
@@ -61,11 +61,11 @@ namespace Orbital.Mock.Server.Middleware
             response.Body.Seek(0, SeekOrigin.Begin);
             string text = await new StreamReader(response.Body).ReadToEndAsync();
             response.Body.Seek(0, SeekOrigin.Begin);
-            return $"{response.StatusCode}: {text}";
+            return $"{response.Headers} {response.StatusCode}: {text}";
         }
 
 
     }
 
-    }
+}
 
