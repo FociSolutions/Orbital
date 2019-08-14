14fb14116dea62d4dcb048cff4fc2d36c6a59b09
Alex Yorke <alex.yorke@focisolutions.com>
2019-08-14 14:25:30 -0400

    Fix all linting errors causing the Azure CI build to fail

--- src/orbital-designer/src/app/services/orbital-server.service.spec.ts
+++ src/orbital-designer/src/app/services/orbital-server.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OrbitalServerService } from './orbital-server.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('OrbitalServerService', () => {
   beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientTestingModule]}));
