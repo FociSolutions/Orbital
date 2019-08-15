8527f445059bc74cea4e51140f5c158135f1641c
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-08-15 10:55:02 -0400

    added integration test project

---
+++ tests/Orbital.Mock.Server.IntegrationTests/OrbitalAdmin/GetMockDefinitions/GetMockDefinitionsFixture.cs
@@ -0,0 +1,40 @@
+﻿using LightBDD.XUnit2;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using Xunit;
+
+[assembly: LightBddScope]
+namespace Orbital.Mock.Server.IntegrationTests.OrbitalAdmin.GetMockDefinitions
+{
+    public partial class GetMockDefinitions_Feature : FeatureFixture
+    {
+        public GetMockDefinitions_Feature()
+        {
+            Client = new HttpClient();
+        }
+
+        private HttpClient Client;
+
+        private HttpResponseMessage GetAllResult;
+
+        public object Uri { get; }
+
+        private void When_the_client_request_all_mock_definitions()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, CommonData.ServerBaseUri);
+            GetAllResult = Client.SendAsync(request).Result;
+        }
+
+        private void Then_the_getall_operation_should_be_successful()
+        {
+            Assert.True(GetAllResult.IsSuccessStatusCode);
+        }
+
+        private void Then_the_getall_operation_returned_empty_list()
+        {
+            var content = GetAllResult.Content.ReadAsStringAsync().Result;
+            var result = JArray.Parse(content);
+            Assert.Empty(result);
+        }
+    }
+}
