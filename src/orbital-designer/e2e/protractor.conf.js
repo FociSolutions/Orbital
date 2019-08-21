4af4eb727296e1258b021b5511bfc4df48e6e4d1
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-08-21 12:08:24 -0400

    copied e2e files over

--- src/orbital-designer/e2e/protractor.conf.js
+++ src/orbital-designer/e2e/protractor.conf.js
@@ -3,7 +3,9 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { JUnitXmlReporter } = require('jasmine-reporters');
 
+process.env.CHROME_BIN = process.env.CHROME_BIN || require("puppeteer").executablePath();
 /**
  * @type { import("protractor").Config }
  */
@@ -13,7 +15,12 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu", "--window-size=1200,900"],
+      binary: process.env.CHROME_BIN
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -28,5 +35,10 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    var junitReporter = new JUnitXmlReporter({
+      savePath: require('path').join(__dirname, './junit'),
+      consolidateAll: true
+    });
+    jasmine.getEnv().addReporter(junitReporter);
   }
 };
