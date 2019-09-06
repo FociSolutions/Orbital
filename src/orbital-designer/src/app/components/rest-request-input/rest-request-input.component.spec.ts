ecb896bc778976d38819a8b110f3569e415feaa3
Alex Yorke <alex.yorke@focisolutions.com>
2019-09-06 14:13:08 -0400

    Add initial test for rest request input

--- src/orbital-designer/src/app/components/rest-request-input/rest-request-input.component.spec.ts
+++ src/orbital-designer/src/app/components/rest-request-input/rest-request-input.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RestRequestInputComponent } from './rest-request-input.component';
+import { OrbitalCommonModule } from '../orbital-common/orbital-common.module';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { promise } from 'protractor';
+import { doesNotThrow } from 'assert';
 
 describe('RestRequestInputComponent', () => {
   let component: RestRequestInputComponent;
@@ -8,9 +16,15 @@ describe('RestRequestInputComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RestRequestInputComponent ]
-    })
-    .compileComponents();
+      declarations: [RestRequestInputComponent],
+      imports: [
+        OrbitalCommonModule,
+        MatCardModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        RouterTestingModule.withRoutes([])
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +35,14 @@ describe('RestRequestInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should send a successful get request to localhost', async () => {
+    await component
+      .sendRequest('http://localhost:4200')
+      .toPromise()
+      .then(result => {
+        expect(result).toBeTruthy();
+      });
   });
 });
