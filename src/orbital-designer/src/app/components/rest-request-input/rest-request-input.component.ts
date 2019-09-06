3b34c63b40eb3e6c4686cd4708d40a2c0c2e2897
Alex Yorke <alex.yorke@focisolutions.com>
2019-09-06 11:51:34 -0400

    Create field for url with validation

--- src/orbital-designer/src/app/components/rest-request-input/rest-request-input.component.ts
+++ src/orbital-designer/src/app/components/rest-request-input/rest-request-input.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-rest-request-input',
-  templateUrl: './rest-request-input.component.html',
-  styleUrls: ['./rest-request-input.component.scss']
+  selector: "app-rest-request-input",
+  templateUrl: "./rest-request-input.component.html",
+  styleUrls: ["./rest-request-input.component.scss"]
 })
 export class RestRequestInputComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  formGroup: FormGroup;
+  constructor(private router: Router, private location: Location) {
+    const urlPattern = /^(http[s]?:\/\/)/;
+    this.formGroup = new FormGroup({
+      uri: new FormControl(
+        "",
+        Validators.compose([
+          Validators.maxLength(2048),
+          Validators.required,
+          Validators.pattern(urlPattern)
+        ])
+      )
+    });
   }
 
+  errorMessage(controlkey: string): string {
+    const errors = this.formGroup.controls[controlkey].errors;
+    if (!errors) {
+      return "";
+    }
+
+    const errorMessages = {
+      required: `${controlkey} is required`,
+      maxlength: "Max characters exceeded"
+    };
+
+    const errorMessage = Object.keys(errors)
+      .map(err => (!!errorMessages[err] ? errorMessages[err] : "Invalid Input"))
+      .join("\n");
+
+    return errorMessage.trim();
+  }
 }
