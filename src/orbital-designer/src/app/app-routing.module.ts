4ed1865b2339581a9764b4b7bd321d344e7fbfc7
Alex Yorke <alex.yorke@focisolutions.com>
2019-09-09 14:34:15 -0400

    Add route

--- src/orbital-designer/src/app/app-routing.module.ts
+++ src/orbital-designer/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { OverviewRedirectService } from './services/overview-redirect.service';
 import { HomeViewComponent } from './components/home-view/home-view.component';
 import { CreateNewMockViewComponent } from './components/create-new-mock-view/create-new-mock-view.component';
+import { ImportFromFileComponent } from './components/import-from-file/import-from-file.component';
 
 const routes: Routes = [
   { path: '', component: HomeViewComponent },
   { path: 'create-new-mock', component: CreateNewMockViewComponent },
+  { path: 'import-from-file', component: ImportFromFileComponent },
   { path: '**', component: HomeViewComponent }
 ];
 
