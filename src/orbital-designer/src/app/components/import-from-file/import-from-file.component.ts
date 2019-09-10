f410cafe97fe1df1c90a0bf0ee3ce0f00299ad8e
Alex Yorke <alex.yorke@focisolutions.com>
2019-09-10 09:47:24 -0400

    Make upload file appear in list with validation
    This does not include adding the file to the service

--- src/orbital-designer/src/app/components/import-from-file/import-from-file.component.ts
+++ src/orbital-designer/src/app/components/import-from-file/import-from-file.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { openApiFileValidator } from '../../../../src/app/validators/async/async-file-content-validator';
 
 @Component({
   selector: 'app-import-from-file',
@@ -7,15 +8,37 @@ import { FormGroup, Validators, FormControl } from '@angular/forms';
   styleUrls: ['./import-from-file.component.scss']
 })
 export class ImportFromFileComponent implements OnInit {
-  files: object[] = [
-    { title: 'MockDefinition1', error: 'Invalid File' },
-    { title: 'MockDefinition2' }
-  ];
   formGroup: FormGroup;
   constructor() {
     this.formGroup = new FormGroup({
-      openApiFile: new FormControl(null, Validators.required)
+      openApiFile: new FormControl(
+        null,
+        Validators.required,
+        openApiFileValidator
+      )
     });
+  }
+
+  /**
+   * Function used to get the appropriate error messages to display for the form control
+   * @param controlkey The key of the control to get the errors from
+   */
+  errorMessages(controlkey: string): string[] {
+    const errors = this.formGroup.controls[controlkey].errors;
+    if (!errors) {
+      return [];
+    }
+
+    const defaultErrorMappings = {
+      required: `${controlkey} is required`,
+      maxlength: 'Max characters exceeded'
+    };
+
+    const errorMessages = Object.keys(errors).map(err =>
+      !!defaultErrorMappings[err] ? defaultErrorMappings[err] : errors[err]
+    );
+
+    return errorMessages;
   }
 
   ngOnInit() {}
