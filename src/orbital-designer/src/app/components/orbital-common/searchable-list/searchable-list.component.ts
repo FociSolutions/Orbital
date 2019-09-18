10d8cb1297855aada10eafe7aef29dd114eed435
Tyler Mizuyabu <tyler.mizuyabu@focisolutions.com>
2019-09-18 16:41:09 -0400

    added comments and changed variable names

--- src/orbital-designer/src/app/components/orbital-common/searchable-list/searchable-list.component.ts
+++ src/orbital-designer/src/app/components/orbital-common/searchable-list/searchable-list.component.ts
@@ -25,6 +25,12 @@ export class SearchableListComponent implements OnInit {
   @Input() noSearchResultsMessage = 'No search results found';
   @Input() itemToStringFn: (_: any) => string = x => x as string;
   constructor() {}
+
+  /**
+   * Function that applies the ignore-case starts with string filter to the list of items.
+   * It uses the itemToStringFn property to convert the items into searchable strings to search
+   * against
+   */
   getSearchedList(): any[] {
     if (!!this.input && this.input.nativeElement.value.length > 0) {
       return this.list.filter(item =>
@@ -37,9 +43,16 @@ export class SearchableListComponent implements OnInit {
     return this.list;
   }
 
+  /**
+   * Returns true if the target string starts with the startsWith string. It sets
+   * both of them to lowercase before perfoming the compares in order to ignore case.
+   */
   // tslint:disable-next-line: member-ordering
-  static ignoreCaseStartsWithMatch(left: string, right: string): boolean {
-    return left.toLowerCase().startsWith(right.toLowerCase());
+  static ignoreCaseStartsWithMatch(
+    target: string,
+    startsWith: string
+  ): boolean {
+    return target.toLowerCase().startsWith(startsWith.toLowerCase());
   }
   ngOnInit() {}
 }
