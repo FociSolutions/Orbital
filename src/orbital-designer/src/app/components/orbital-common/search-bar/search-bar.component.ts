66e0213c45b69100d17ebe496fa5a28803c152de
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-09-24 15:18:48 -0400

    added fn for filtering search bar

--- src/orbital-designer/src/app/components/orbital-common/search-bar/search-bar.component.ts
+++ src/orbital-designer/src/app/components/orbital-common/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { MatListOption } from '@angular/material/list';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+  @Output() filteredList: MatListOption[] = [];
+
+  @Input() list: any[] = [];
+  @Input() itemToStringFn: (_: any) => string = x => x as string;
   constructor() {}
 
+  /**
+   * Function that takes a string and a list as input and filters out the matlist
+   * based on the input text
+   * @param value The string to be searched
+   * @param list the list to filter out of
+   */
+
+  onSearchInput(value: string) {
+    this.filteredList = this.list.filter(
+      option =>
+        !SearchBarComponent.ignoreCaseContainsMatch(
+          this.itemToStringFn(option.value),
+          value
+        )
+    );
+  }
+
+  /**
+   * Returns true if the target parameter contains the substring parameter. It sets
+   * both of them to lowercase before performing the compares in order to ignore case.
+   */
+  // tslint:disable-next-line: member-ordering
+  static ignoreCaseContainsMatch(target: string, substring: string): boolean {
+    return target.toLowerCase().includes(substring.toLowerCase());
+  }
   ngOnInit() {}
 }
