5fe9ce969e79463c8e4c587f8a27a6ef86e1bdd2
Alex Yorke <alex.yorke@focisolutions.com>
2019-09-30 16:58:50 -0400

    Fix styling

--- src/orbital-designer/src/app/components/scenario-view/scenario-list-item/scenario-list-item.component.ts
+++ src/orbital-designer/src/app/components/scenario-view/scenario-list-item/scenario-list-item.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { DesignerStore } from 'src/app/store/designer-store';
 import { NGXLogger } from 'ngx-logger';
 import { Scenario } from '../../../models/mock-definition/scenario/scenario.model';
+import * as HttpStatus from 'http-status-codes';
 
+import { STATUS_CODES } from 'http';
 @Component({
   selector: 'app-scenario-list-item',
   templateUrl: './scenario-list-item.component.html',
@@ -17,4 +19,8 @@ export class ScenarioListItemComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  getScenarioResponseStatusString() {
+    return HttpStatus.getStatusText(this.scenario.response.status);
+  }
 }
