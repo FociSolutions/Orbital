bd0701069a8f2572ad194178061cf6a53f959da0
Alex Yorke <alex.yorke@focisolutions.com>
2019-10-03 14:18:26 -0400

    Add case-insensitive pipe tests

--- src/orbital-designer/src/app/pipes/get-endpoint-scenarios/get-endpoint-scenarios.pipe.spec.ts
+++ src/orbital-designer/src/app/pipes/get-endpoint-scenarios/get-endpoint-scenarios.pipe.spec.ts
@@ -42,6 +42,39 @@ describe('GetEndpointScenariosPipe', () => {
     expect(results).toEqual([mockMatchingScenario]);
   });
 
+  it('should return a filtered list of scenarios ignoring verb case', () => {
+    const pipe = new GetEndpointScenariosPipe();
+    const mockPath = faker.internet.url();
+    const mockVerb = faker.random.arrayElement([
+      VerbType.GET,
+      VerbType.DELETE,
+      VerbType.PUT,
+      VerbType.POST
+    ]);
+    const mockMatchingScenario = newScenario(mockVerb, mockPath);
+    const mockNonMatchingScenario = newScenario(
+      faker.random.arrayElement([
+        'gET',
+        'deLetE',
+        'pOST',
+        'PuT'
+      ]),
+      mockPath + faker.random.words()
+    );
+    const mockEndpoint = {
+      path: mockPath,
+      verb: mockVerb,
+      spec: null
+    };
+
+    const results = pipe.transform(
+      [mockMatchingScenario, mockNonMatchingScenario],
+      mockEndpoint
+    );
+
+    expect(results).toEqual([mockMatchingScenario]);
+  });
+
   it('should return an unfiltered list of scenarios if no endpoint is given', () => {
     const pipe = new GetEndpointScenariosPipe();
     const mockPath = faker.internet.url();
