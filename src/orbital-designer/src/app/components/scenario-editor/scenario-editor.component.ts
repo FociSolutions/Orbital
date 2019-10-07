4888e5fde3ad852f0e8aa9d9945e11d76a76531c
Alex Yorke <alex.yorke@focisolutions.com>
2019-10-07 13:47:06 -0400

    Generate empty scenario-editor component

---
+++ src/orbital-designer/src/app/components/scenario-editor/scenario-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scenario-editor',
+  templateUrl: './scenario-editor.component.html',
+  styleUrls: ['./scenario-editor.component.scss']
+})
+export class ScenarioEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
