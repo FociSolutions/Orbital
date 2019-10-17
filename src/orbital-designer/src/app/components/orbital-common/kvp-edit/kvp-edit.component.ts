35df329cbcb9d953fd2fa8602bfdf19d72ec11a4
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-10-17 15:44:28 -0400

    renamed vars

--- src/orbital-designer/src/app/components/orbital-common/kvp-edit/kvp-edit.component.ts
+++ src/orbital-designer/src/app/components/orbital-common/kvp-edit/kvp-edit.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./kvp-edit.component.scss']
 })
 export class KvpEditComponent implements OnInit {
-  @Input() existingKvpMap: Map<string, string> = new Map<string, string>();
+  @Input() kvpMap: Map<string, string> = new Map<string, string>();
   @Input() addKvpTitle: string;
   @Input() listKvpTitle: string;
 
-  @Output() kvpMap = new EventEmitter<Map<string, string>>();
+  @Output() savedKvpMap = new EventEmitter<Map<string, string>>();
 
   ngOnInit() {}
 }
