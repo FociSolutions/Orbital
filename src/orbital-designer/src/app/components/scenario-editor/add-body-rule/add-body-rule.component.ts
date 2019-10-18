2d411f4725658f3c43c2b1708ce7733a5b5a06c4
Alex Yorke <alex.yorke@focisolutions.com>
2019-10-18 10:57:59 -0400

    Connect JSON validator to body value

--- src/orbital-designer/src/app/components/scenario-editor/add-body-rule/add-body-rule.component.ts
+++ src/orbital-designer/src/app/components/scenario-editor/add-body-rule/add-body-rule.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { extendBuiltInValidatorFactory } from 'src/app/validators/extend-built-in-validator-factory/extend-built-in-validator-factory';
 import { openApiFileValidator } from 'src/app/validators/open-api-file-validator/open-api-file-validator';
 import { NGXLogger } from 'ngx-logger';
+import { jsonValidator } from 'src/app/validators/json-validator/json-validator';
 
 @Component({
   selector: 'app-add-body-rule',
@@ -20,17 +21,15 @@ export class AddBodyRuleComponent implements OnInit {
           logger
         )
       ),
-      bodyValue: new FormControl(
-        '',
-        extendBuiltInValidatorFactory(
-          Validators.compose([Validators.required]),
-          logger
-        )
-      )
+      bodyValue: new FormControl()
     });
+
+    // ensure that the body value is valid JSON
+    this.addBodyRuleFormGroup.get('bodyValue').setValidators([
+      Validators.required,
+      jsonValidator()
+    ]);
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
