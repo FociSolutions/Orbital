d1370794f042eb54af210a6bb4cb8d6bdbe06eef
Alex Yorke <alex.yorke@focisolutions.com>
2019-10-22 16:23:17 -0400

    Add Input to shouldDisable

--- src/orbital-designer/src/app/components/scenario-editor/add-body-rule/add-body-rule.component.ts
+++ src/orbital-designer/src/app/components/scenario-editor/add-body-rule/add-body-rule.component.ts
@@ -16,7 +16,7 @@ export class AddBodyRuleComponent implements OnInit {
   bodyRuleTypeValues = BodyRuleType;
   @Input() bodyRules: BodyRule[] = [];
   @Output() bodyRuleOutput: EventEmitter<BodyRule> = new EventEmitter<BodyRule>();
-  shouldDisable = false;
+  @Input() shouldDisable = false;
 
   bodyType: BodyRuleType;
   bodyValue = '';
