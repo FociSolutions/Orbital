488796f0274319837b4a229711a46aafd804a85b
Alex Yorke <alex.yorke@focisolutions.com>
2019-10-25 11:31:05 -0400

    Add ability to show body type and rule in body-rule-list-item component

--- src/orbital-designer/src/app/components/scenario-editor/add-body-rule-container/body-rule-list-item/body-rule-list-item.component.ts
+++ src/orbital-designer/src/app/components/scenario-editor/add-body-rule-container/body-rule-list-item/body-rule-list-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { BodyRuleType } from 'src/app/models/mock-definition/scenario/body-rule.type';
+import { BodyRule } from 'src/app/models/mock-definition/scenario/body-rule.model';
 
 @Component({
   selector: 'app-body-rule-list-item',
@@ -8,14 +9,36 @@ import { BodyRuleType } from 'src/app/models/mock-definition/scenario/body-rule.
 })
 export class BodyRuleListItemComponent implements OnInit {
   bodyRuleTypeValues = BodyRuleType;
-  bodyType: BodyRuleType;
+  @Input() bodyRule: BodyRule;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
 
   deleteBodyRule() {
 
+  }
+
+  /**
+   * Gets the rule from the body; returns an empty string if undefined or null
+   */
+  getBodyRule() {
+    if (!!this.bodyRule.rule) {
+      return JSON.stringify(this.bodyRule.rule);
+    } else {
+      return '';
+    }
+  }
+
+  /**
+   * Gets the type from the body; returns an empty string if undefined or null
+   */
+  getBodyType() {
+    if (!!this.bodyRule.type) {
+      return this.bodyRule.type;
+    } else {
+      return '';
+    }
   }
 }
