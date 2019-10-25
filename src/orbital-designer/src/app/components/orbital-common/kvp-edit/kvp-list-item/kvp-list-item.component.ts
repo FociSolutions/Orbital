6e8a951723a8ebc15346aa89980c0937ae42d82f
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-10-25 16:54:49 -0400

    End of week push

--- src/orbital-designer/src/app/components/orbital-common/kvp-edit/kvp-list-item/kvp-list-item.component.ts
+++ src/orbital-designer/src/app/components/orbital-common/kvp-edit/kvp-list-item/kvp-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'app-kvp-list-item',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kvp-list-item.component.scss']
 })
 export class KvpListItemComponent implements OnInit {
+  @Input() kvp: KeyValue<string, string>;
 
-  constructor() { }
+  key: string;
+  value: string;
 
-  ngOnInit() {
+  @Output() removeKvp: EventEmitter<
+    KeyValue<string, string>
+  > = new EventEmitter<KeyValue<string, string>>();
+
+  constructor() {}
+
+  /**
+   * Emits a removes event with the KeyValue for the parent to remove
+   */
+  onRemove() {
+    this.removeKvp.emit(this.kvp);
   }
 
+  ngOnInit() {
+    this.key = this.kvp.key;
+    this.value = this.kvp.value;
+  }
 }
