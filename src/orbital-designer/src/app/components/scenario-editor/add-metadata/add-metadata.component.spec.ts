3beb333d916e7f10f3722f225796194cbe306c49
Alex Yorke <alex.yorke@focisolutions.com>
2019-10-29 13:19:33 -0400

    Add add-metadata card title and card template

--- src/orbital-designer/src/app/components/scenario-editor/add-metadata/add-metadata.component.spec.ts
+++ src/orbital-designer/src/app/components/scenario-editor/add-metadata/add-metadata.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddMetadataComponent } from './add-metadata.component';
+import { MatExpansionModule, MatFormFieldModule, MatInputModule, MatSelectModule,
+  MatCardModule, MatIconModule, MatDividerModule, MatCheckboxModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoggerTestingModule } from 'ngx-logger/testing';
 
 describe('AddMetadataComponent', () => {
   let component: AddMetadataComponent;
@@ -8,7 +13,18 @@ describe('AddMetadataComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddMetadataComponent ]
+      declarations: [ AddMetadataComponent ],
+      imports: [MatExpansionModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatCardModule,
+        MatIconModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        LoggerTestingModule,
+        MatDividerModule,
+        MatCheckboxModule]
     })
     .compileComponents();
   }));
