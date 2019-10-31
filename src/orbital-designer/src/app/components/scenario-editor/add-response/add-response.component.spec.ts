f6bbe137efc1002ddd29caa9c2bf7b206d5d5ca5
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-10-31 16:52:46 -0400

    added tests

--- src/orbital-designer/src/app/components/scenario-editor/add-response/add-response.component.spec.ts
+++ src/orbital-designer/src/app/components/scenario-editor/add-response/add-response.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddResponseComponent } from './add-response.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OrbitalCommonModule } from '../../orbital-common/orbital-common.module';
+import * as faker from 'faker';
 
 describe('AddResponseComponent', () => {
   let component: AddResponseComponent;
@@ -22,5 +23,22 @@ describe('AddResponseComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('addResponse.setStatusCode', () => {
+    it('should expect the corresponding status code message in the status field if the status code is valid ', () => {
+      component.statusCode = '200';
+      expect(component.statusMessage).toEqual('OK');
+    });
+
+    it('should expect the corresponding status code message in the status field if the status code is invalid ', () => {
+      component.statusCode = '';
+      expect(component.statusMessage).toEqual('Enter a Status Code');
+    });
+
+    it('should expect the corresponding status code message in the status field if the status code is empty', () => {
+      component.statusCode = faker.lorem.words();
+      expect(component.statusMessage).toEqual('Invalid Status Code');
+    });
   });
 });
