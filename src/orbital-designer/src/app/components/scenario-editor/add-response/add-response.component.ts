503c6e0204cd7d1fb8b41d48b8fca1c929ead1ff
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-10-31 15:36:12 -0400

    Added respose message:

--- src/orbital-designer/src/app/components/scenario-editor/add-response/add-response.component.ts
+++ src/orbital-designer/src/app/components/scenario-editor/add-response/add-response.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import * as HttpStatus from 'http-status-codes';
 
 @Component({
   selector: 'app-add-response',
@@ -12,10 +13,40 @@ export class AddResponseComponent implements OnInit {
   @Output() responseOutput: EventEmitter<Response>;
   @Output() isValid: EventEmitter<boolean>;
 
+  /**
+   * The locally store status code
+   */
+  statusCodeEntered: string;
+
+  get statusCode(): string {
+    return this.statusCodeEntered;
+  }
+
+  /**
+   * This setter checks to see if the status code field is empty and valid,
+   * if it is valid, if parses the string to a number and retrieves the corresponding
+   * status code
+   */
+  set statusCode(statusCode: string) {
+    if (statusCode === '') {
+      this.statusMessage = 'Enter a Status Code';
+    } else {
+      try {
+        this.statusMessage = HttpStatus.getStatusText(+statusCode);
+      } catch (Error) {
+        this.statusMessage = 'Invalid Status Code';
+      }
+    }
+  }
+  statusMessage: string;
+
   constructor() {
     this.responseOutput = new EventEmitter<Response>();
     this.isValid = new EventEmitter<boolean>();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.statusMessage = '';
+    this.statusCode = '';
+  }
 }
