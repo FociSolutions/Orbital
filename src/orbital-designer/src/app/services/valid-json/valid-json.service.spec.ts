75a4afeb7339785f8d0787f26a1f47329d9a7cb3
Adrian Ramcharitar <adrian.ramcharitar@focisolutions.com>
2019-11-04 15:26:22 -0500

    Added more tests

--- src/orbital-designer/src/app/services/valid-json/valid-json.service.spec.ts
+++ src/orbital-designer/src/app/services/valid-json/valid-json.service.spec.ts
@@ -19,10 +19,26 @@ describe('add-body-rule.isValidJSON', () => {
   });
 });
 
+describe('add-body-rule.isValidJSON', () => {
+  it('should return true if the JSON can be parsed ', () => {
+    const service: ValidJsonService = TestBed.get(ValidJsonService);
+    const Actual = service.isValidJSON('{}');
+    expect(Actual).toBe(true);
+  });
+});
+
 describe('add-body-rule.tryParseJSON', () => {
   it('should return null if the JSON cannot be parsed ', () => {
     const service: ValidJsonService = TestBed.get(ValidJsonService);
     const Actual = service.tryParseJSON('invalid');
     expect(Actual).toBe(null);
+  });
+
+  describe('add-body-rule.tryParseJSON', () => {
+    it('should return a json object if the JSON can be parsed ', () => {
+      const service: ValidJsonService = TestBed.get(ValidJsonService);
+      const Actual = service.tryParseJSON('{}');
+      expect(Actual).toEqual({});
+    });
   });
 });
