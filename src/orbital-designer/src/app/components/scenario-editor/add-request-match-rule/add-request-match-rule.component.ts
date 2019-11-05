79a20dbe540195ca734b809f604dd9e01405f282
Alex Yorke <alex.yorke@focisolutions.com>
2019-11-05 16:17:20 -0500

    Emit copy of request match rules when saving

--- src/orbital-designer/src/app/components/scenario-editor/add-request-match-rule/add-request-match-rule.component.ts
+++ src/orbital-designer/src/app/components/scenario-editor/add-request-match-rule/add-request-match-rule.component.ts
@@ -52,7 +52,7 @@ export class AddRequestMatchRuleComponent implements OnInit {
     if (shouldSave) {
       // validate request match rules
       if (!!this.headerMatchRules && !!this.queryMatchRules && !!this.bodyMatchRules) {
-        this.requestMatchRuleOutput.emit(this.requestMatchRule);
+        this.requestMatchRuleOutput.emit(JSON.parse(JSON.stringify(this.requestMatchRule)));
         this.logger.debug('The request match rules have been emitted', this.requestMatchRule);
         this.canCollapseCard = true;
       }
