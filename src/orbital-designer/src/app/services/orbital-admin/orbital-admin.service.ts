cb847094fd474355be5c07fa579d3cc1227f3ddd
Maria reyes <maria.reyes@focisolutions.com>
2019-12-17 13:26:52 -0500

    remove store

--- src/orbital-designer/src/app/services/orbital-admin/orbital-admin.service.ts
+++ src/orbital-designer/src/app/services/orbital-admin/orbital-admin.service.ts
@@ -3,18 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { NGXLogger } from 'ngx-logger';
 import { MockDefinition } from 'src/app/models/mock-definition/mock-definition.model';
 import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import Json from '../../../app/models/json';
 import { cloneDeep } from 'lodash';
-import { DesignerStore } from 'src/app/store/designer-store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrbitalAdminService {
   constructor(private httpClient: HttpClient,
-              private logger: NGXLogger,
-              private store: DesignerStore) {}
+              private logger: NGXLogger) {}
 
   /**
    * POSTs a Mockdefinition to the server
@@ -58,7 +56,7 @@ export class OrbitalAdminService {
   }
 
 /**
- * Removes the specified mock definition from the designer store and the orbital server
+ * Removes the specified mock definition from the orbital server
  *
  * @param url the orbital server url
  * @param mockDefId the title of the mock definition that will be removed
