5c029b0ce6e1c84d3d860e0ae6a545e5d9157232
Maria reyes <maria.reyes@focisolutions.com>
2019-12-17 13:27:12 -0500

    remove store from spec

--- src/orbital-designer/src/app/services/orbital-admin/orbital-admin.service.spec.ts
+++ src/orbital-designer/src/app/services/orbital-admin/orbital-admin.service.spec.ts
@@ -2,13 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { LoggerTestingModule } from 'ngx-logger/testing';
 import { OrbitalAdminService } from './orbital-admin.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { DesignerStore } from 'src/app/store/designer-store';
 
 describe('OrbitalAdminService', () => {
   let service: OrbitalAdminService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [OrbitalAdminService, DesignerStore],
+      providers: [OrbitalAdminService],
       imports: [HttpClientTestingModule, LoggerTestingModule]
     });
     service = TestBed.get(OrbitalAdminService);
