4d0411776f49befea24235451452bec80e513adb
Maria reyes <maria.reyes@focisolutions.com>
2020-01-08 16:50:46 -0500

    add static class and unit tests

---
+++ tests/Orbital.Mock.Server.Tests/Pipelines/Comparers/RegexComparerTests.cs
@@ -0,0 +1,31 @@
+﻿using Orbital.Mock.Server.Pipelines.Comparers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orbital.Mock.Server.Tests.Pipelines.Comparers
+{
+    public class RegexComparerTests
+    {
+        [Fact]
+        public void SuccessRegularExpressionComparison()
+        {
+            var regex = "[ae]";
+            var valueToEvaluate = "gray";
+            var actual = RegexComparer.Compare(regex, valueToEvaluate);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void FailureRegularExpressionComparison()
+        {
+            var regex = "[ae]";
+            var valueToEvaluate = "box";
+            var actual = RegexComparer.Compare(regex, valueToEvaluate);
+
+            Assert.False(actual);
+        }
+    }
+}
