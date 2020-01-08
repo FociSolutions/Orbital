0d0cfb56ad86dbf829ff05464e5c96857f009d00
Maria reyes <maria.reyes@focisolutions.com>
2020-01-08 16:53:33 -0500

    small change

--- src/Orbital.Mock.Server/Pipelines/Comparers/RegexComparer.cs
+++ src/Orbital.Mock.Server/Pipelines/Comparers/RegexComparer.cs
@@ -10,7 +10,7 @@ namespace Orbital.Mock.Server.Pipelines.Comparers
         /// <param name="regex">regular expression used to find a match</param>
         /// <param name="requestToEvalute">value to be used against regular expression</param>
         /// <returns>a flag indicating if a match was found on requestToEvalute</returns>
-        public static bool Compare(string regex, string requestToEvalute)
+        public static bool Compare(string requestToEvalute, string regex)
         {
             return Regex.IsMatch(requestToEvalute, regex);
         }
