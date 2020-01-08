d00cc97d458428c75749542a12a079b59d1992ed
Maria reyes <maria.reyes@focisolutions.com>
2020-01-08 16:50:46 -0500

    add static class and unit tests

---
+++ src/Orbital.Mock.Server/Pipelines/Comparers/RegexComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Orbital.Mock.Server.Pipelines.Comparers
+{
+    public static class RegexComparer
+    {
+        /// <summary>
+        /// This comparer evaluates if the regular expression provided finds a match in the requestToEvalute value. 
+        /// </summary>
+        /// <param name="regex">regular expression used to find a match</param>
+        /// <param name="requestToEvalute">value to be used against regular expression</param>
+        /// <returns>a flag indicating if a match was found on requestToEvalute</returns>
+        public static bool Compare(string regex, string requestToEvalute)
+        {
+            return Regex.IsMatch(requestToEvalute, regex);
+        }
+    }
+}
