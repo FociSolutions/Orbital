be3d25f846065fdcbfb152ad0c1c015789e168fe
Jonathan Cockrem <jonathan.cockrem@focisolutions.com>
2020-01-09 10:52:20 -0500

    Couple of more summary comments

--- src/Orbital.Mock.Server/Models/Interfaces/IRule.cs
+++ src/Orbital.Mock.Server/Models/Interfaces/IRule.cs
@@ -1,10 +1,16 @@
 ﻿namespace Orbital.Mock.Server.Models.Interfaces
 {
+    /// <summary>
+    /// Interface to implement comparer types
+    /// </summary>
     public interface IRule
     {
         ComparerType Type { get; set; }
     }
 
+    /// <summary>
+    /// Enum that indicates which comparer to use
+    /// </summary>
     public enum ComparerType
     {
         Regex = 0,
