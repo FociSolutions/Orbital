e8ea7293c53f03b1c808a7eaef14145930f4ebb0
Jonathan Cockrem <jonathan.cockrem@focisolutions.com>
2020-01-09 10:49:47 -0500

    Updating comments

--- src/Orbital.Mock.Server/Pipelines/RuleMatchers/Interfaces/IRuleMatcher.cs
+++ src/Orbital.Mock.Server/Pipelines/RuleMatchers/Interfaces/IRuleMatcher.cs
@@ -4,8 +4,14 @@ using Orbital.Mock.Server.Pipelines.Ports.Interfaces;
 
 namespace Orbital.Mock.Server.Pipelines.RuleMatchers.Interfaces
 {
+    /// <summary>
+    /// Interface contains method signature of matching method
+    /// </summary>
+    /// <typeparam name="T">Comparer rule type</typeparam>
+    /// <typeparam name="R">Input for matching</typeparam>
     public interface IRuleMatcher<T,R> where T:IRule where R:IMatchPort
     {
+
         MatchResult Match(T rule, R input);
     }
 }
--- src/Orbital.Mock.Server/Pipelines/RuleMatchers/RuleMatcher.cs
+++ src/Orbital.Mock.Server/Pipelines/RuleMatchers/RuleMatcher.cs
@@ -5,8 +5,17 @@ using System.Collections.Generic;
 
 namespace Orbital.Mock.Server.Pipelines.RuleMatchers
 {
+    /// <summary>
+    /// Abstract class for matching 
+    /// </summary>
     abstract public class RuleMatcher
     {
+        /// <summary>
+        /// Match method that returns a collection of MatchResult
+        /// </summary>
+        /// <param name="T">Comparer rule type</param>
+        /// <param name="R">Input for matching</param>
+        /// <returns></returns>
         public abstract IEnumerable<MatchResult> Match(IRule T, IMatchPort R);
     }
 }
