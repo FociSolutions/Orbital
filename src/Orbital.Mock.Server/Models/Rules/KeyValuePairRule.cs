eeedd01ef313cd9efcc1441ee1027b7cc8c01c44
Maria reyes <maria.reyes@focisolutions.com>
2020-01-10 09:59:30 -0500

    remove unnecesarry IEquality comparer

--- src/Orbital.Mock.Server/Models/Rules/KeyValuePairRule.cs
+++ src/Orbital.Mock.Server/Models/Rules/KeyValuePairRule.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Orbital.Mock.Server.Models.Rules
 {
-    public class KeyValuePairRule : IRule, IEqualityComparer<KeyValuePair<string, string>>
+    public class KeyValuePairRule : IRule
     {
         /// <summary>
         /// KeyValuePairRule Constructor With Parameters
@@ -19,14 +19,5 @@ namespace Orbital.Mock.Server.Models.Rules
 
         public ComparerType Type { get; set; }
 
-        public bool Equals(KeyValuePair<string, string> rulePair, KeyValuePair<string, string> requestPair)
-        {
-            return rulePair.Value == requestPair.Value;
-        }
-
-        public int GetHashCode(KeyValuePair<string, string> obj)
-        {
-            return obj.Key.GetHashCode();
-        }
     }
 }
