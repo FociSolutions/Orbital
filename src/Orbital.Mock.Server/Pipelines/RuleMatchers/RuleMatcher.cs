8f311a63dbaab7970a3406338743c23f97ca61b3
Maria reyes <maria.reyes@focisolutions.com>
2020-01-14 15:12:05 -0500

    change rulematcher

--- src/Orbital.Mock.Server/Pipelines/RuleMatchers/RuleMatcher.cs
+++ src/Orbital.Mock.Server/Pipelines/RuleMatchers/RuleMatcher.cs
@@ -1,4 +1,6 @@
 ﻿using Orbital.Mock.Server.Models;
+using Orbital.Mock.Server.Models.Interfaces;
+using Orbital.Mock.Server.Pipelines.Comparers;
 using Orbital.Mock.Server.Pipelines.RuleMatchers.Interfaces;
 
 namespace Orbital.Mock.Server.Pipelines.RuleMatchers
@@ -6,9 +8,30 @@ namespace Orbital.Mock.Server.Pipelines.RuleMatchers
     /// <summary>
     /// Abstract class for matching 
     /// </summary>
-    abstract class RuleMatcher : IRuleMatcher
+    public class RuleMatcher : IRuleMatcher
     {
         /// <inheritdoc/>
-        public abstract bool Match(Assert[] asserts);
+        public bool Match(Assert[] asserts)
+        {
+            var isMatch = false;
+            foreach(var assert in asserts)
+            {
+                switch (assert.Rule)
+                {
+                    case ComparerType.Regex:
+                       isMatch = RegexComparer.Compare(assert.Actual, assert.Expect);
+                        break;
+                    case ComparerType.Contains:
+                        break;
+                    case ComparerType.StartWith:
+                        break;
+                    case ComparerType.EndWith:
+                        break;
+                    case ComparerType.Equal:
+                        break;
+                }
+            }
+            return isMatch;
+        }
     }
 }
