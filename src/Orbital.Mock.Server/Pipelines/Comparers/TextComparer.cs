d3f6be91cb7524ac15c04694c8ca1f6e725d335d
Maria reyes <maria.reyes@focisolutions.com>
2020-01-20 14:27:22 -0500

    address changes

--- src/Orbital.Mock.Server/Pipelines/Comparers/TextComparer.cs
+++ src/Orbital.Mock.Server/Pipelines/Comparers/TextComparer.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// This comparer evaluates if the rule equals the request string. 
         /// </summary>
-        /// <param name="rule">the rule to prove the request is valid</param>
+        /// <param name="rule">the rule to prove if the request is valid</param>
         /// <param name="requestToEvaluate">value to be matched against the rule</param>
         /// <returns>whether a match was found with requestToEvalute and rule provided</returns>
         public static bool Equals(string requestToEvaluate, string rule)
@@ -16,7 +16,7 @@
         /// <summary>
         /// This comparer evaluates if the rule contains the request string. 
         /// </summary>
-        /// <param name="rule">the rule to prove the request is valid</param>
+        /// <param name="rule">the rule to prove if the request is valid</param>
         /// <param name="requestToEvaluate">value to be matched against the rule</param>
         /// <returns>whether a match was found with requestToEvalute and rule provided</returns>
         public static bool Contains(string requestToEvaluate, string rule)
@@ -38,7 +38,7 @@
         /// <summary>
         /// This comparer evaluates if the rule ends with the request string. 
         /// </summary>
-        /// <param name="rule">the rule to prove the request is valid</param>
+        /// <param name="rule">the rule to prove if the request is valid</param>
         /// <param name="requestToEvaluate">value to be matched against the rule</param>
         /// <returns>whether a match was found with requestToEvalute and rule provided</returns>
         public static bool EndsWith(string requestToEvaluate, string rule)
