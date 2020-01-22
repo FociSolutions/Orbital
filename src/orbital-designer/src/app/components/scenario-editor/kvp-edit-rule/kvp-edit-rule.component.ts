33cb90a55f2a9b18270ef02e83373f0e71c748b4
Alex Yorke <alex.yorke@focisolutions.com>
2020-01-22 14:33:37 -0500

    Remove more unused imports

--- src/orbital-designer/src/app/components/scenario-editor/kvp-edit-rule/kvp-edit-rule.component.ts
+++ src/orbital-designer/src/app/components/scenario-editor/kvp-edit-rule/kvp-edit-rule.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
 import { KeyValuePairType } from 'src/app/models/mock-definition/scenario/key-value-pair-type.model';
-import { RuleType } from 'src/app/models/mock-definition/scenario/rule.type';
-import { KeyValueIndexSig } from 'src/app/models/mock-definition/scenario/key-value-index-sig.model';
 
 @Component({
   selector: 'app-kvp-edit-rule',
