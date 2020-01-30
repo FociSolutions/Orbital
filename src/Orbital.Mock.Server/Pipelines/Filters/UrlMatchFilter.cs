9a7e729d5ca92361a6634bfa84c9bc2156612142
adrianramcharitar <adrian.ramcharitar@focisolutions.com>
2020-01-30 11:26:30 -0500

    Created Filter class

---
+++ src/Orbital.Mock.Server/Pipelines/Filters/UrlMatchFilter.cs
@@ -0,0 +1,25 @@
+﻿using Orbital.Mock.Server.Factories.Interfaces;
+using Orbital.Mock.Server.Pipelines.Filters.Bases;
+using Orbital.Mock.Server.Pipelines.Ports.Interfaces;
+using Orbital.Mock.Server.Pipelines.RuleMatchers.Interfaces;
+
+namespace Orbital.Mock.Server.Pipelines.Filters
+{
+    public class UrlMatchFilter<T> : FaultableBaseFilter<T>
+        where T : IFaultablePort, IURLMatchPort, IScenariosPort, IPathValidationPort
+    {
+        private IAssertFactory assertFactory;
+        private IRuleMatcher ruleMatcher;
+
+        public UrlMatchFilter(IAssertFactory assertFactory, IRuleMatcher ruleMatcher)
+        {
+            this.assertFactory = assertFactory;
+            this.ruleMatcher = ruleMatcher;
+        }
+
+        public override T Process(T port)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
