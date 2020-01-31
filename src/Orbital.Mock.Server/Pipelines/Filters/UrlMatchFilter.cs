5a36b3f408226bde1cbd8a266a74c41f46e2bfa7
Adrian <adrian.ramcharitar@focisolutions.com>
2020-01-31 10:21:40 -0500

    br

--- src/Orbital.Mock.Server/Pipelines/Filters/UrlMatchFilter.cs
+++ src/Orbital.Mock.Server/Pipelines/Filters/UrlMatchFilter.cs
@@ -2,6 +2,7 @@
 using Orbital.Mock.Server.Pipelines.Filters.Bases;
 using Orbital.Mock.Server.Pipelines.Ports.Interfaces;
 using Orbital.Mock.Server.Pipelines.RuleMatchers.Interfaces;
+using System.Linq;
 
 namespace Orbital.Mock.Server.Pipelines.Filters
 {
@@ -19,7 +20,31 @@ namespace Orbital.Mock.Server.Pipelines.Filters
 
         public override T Process(T port)
         {
-            throw new System.NotImplementedException();
+            if (!IsPipelineValid(ref port, GetType())) return port;
+
+            foreach (var scenario in port.Scenarios)
+            {
+                foreach (var rule in scenario.RequestMatchRules.HeaderRules)
+                {
+                    var assertsList = assertFactory.CreateAssert(rule, port.Path);
+                    if (!assertsList.Any())
+                    {
+                        port.HeaderMatchResults.Add(new MatchResult(MatchResultType.Ignore, scenario.Id));
+                    }
+                    else
+                    {
+                        port.HeaderMatchResults.Add(ruleMatcher.Match(assertsList.ToArray())
+                                     ? new MatchResult(MatchResultType.Success, scenario.Id)
+                                     : new MatchResult(MatchResultType.Fail, scenario.Id));
+                    }
+
+                }
+
+
+            }
+
+            return port;
         }
     }
 }
+   
