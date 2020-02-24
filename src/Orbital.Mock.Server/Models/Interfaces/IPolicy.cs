563d5ff1ac0dbb65715af4c8ea47e61d178b2bd1
Maria reyes <maria.reyes@focisolutions.com>
2020-02-24 11:14:09 -0500

    add policy enums in client and server

---
+++ src/Orbital.Mock.Server/Models/Interfaces/IPolicy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbital.Mock.Server.Models.Interfaces
+{
+    interface IPolicy
+    {
+        PolicyType Type { get; set; }
+    }
+
+    /// <summary>
+    /// Enum that indicates which policy to use
+    /// </summary>
+    public enum PolicyType
+    {
+        NONE = 0,
+        DELAYRESPONSE
+    };
+}
