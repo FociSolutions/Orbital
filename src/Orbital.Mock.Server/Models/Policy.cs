9027a85dd9317f19b27b3394910159b233ab8e99
Maria reyes <maria.reyes@focisolutions.com>
2020-02-24 14:07:15 -0500

    add policy class

---
+++ src/Orbital.Mock.Server/Models/Policy.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Orbital.Mock.Server.Models.Converters;
+using Orbital.Mock.Server.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Orbital.Mock.Server.Models
+{
+    public class Policy : IEquatable<Policy>, IPolicy
+    {
+        [JsonConverter(typeof(KeyValueConverter))]
+        [JsonProperty("attributes")]
+        public KeyValuePair<string, string> Attributes { get; set; }
+
+        [JsonProperty("type")]
+        public PolicyType Type { get; set; }
+
+        public bool Equals(Policy other)
+        {
+            return other != null &&
+                this.Type == other.Type &&
+                this.Attributes.Equals(other.Attributes);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Type, Attributes);
+        }
+    }
+}
