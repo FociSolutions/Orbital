f3f74ce4a8d7f700c6eb35e416bebb9d3b635209
Alex Yorke <alex.yorke@focisolutions.com>
2020-02-26 18:19:03 -0500

    Create PolicyExecuter class

---
+++ src/Orbital.Mock.Server/Pipelines/Policies/PolicyExecuter.cs
@@ -0,0 +1,23 @@
+﻿using Orbital.Mock.Server.Models;
+using Orbital.Mock.Server.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbital.Mock.Server.Pipelines.Policies
+{
+    public class PolicyExecuter
+    {
+        private bool ExecutePolicy(Policy policy)
+        {
+            switch (policy.Type) {
+                case PolicyType.DELAYRESPONSE:
+                    int millisecondDelay = Convert.ToInt32(policy.Attributes["delay"]);
+                    return DelayPolicy.Execute(millisecondDelay / 1000);
+                default:
+                    return true;
+            }
+        }
+    }
+}
