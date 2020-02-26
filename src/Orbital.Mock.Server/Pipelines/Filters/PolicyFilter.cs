2e0d084900ac73ae09a95d3f3dc9e00bb6f9a91b
Alex Yorke <alex.yorke@focisolutions.com>
2020-02-26 11:56:05 -0500

    Create PolicyFilter and IPolicyPort

---
+++ src/Orbital.Mock.Server/Pipelines/Filters/PolicyFilter.cs
@@ -0,0 +1,15 @@
+﻿using Orbital.Mock.Server.Pipelines.Filters.Bases;
+using Orbital.Mock.Server.Pipelines.Ports.Interfaces;
+using System;
+
+namespace Orbital.Mock.Server.Pipelines.Filters
+{
+    public class PolicyFilter<T> : FaultableBaseFilter<T>
+        where T : IFaultablePort
+    {
+        public override T Process(T port)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Orbital.Mock.Server/Pipelines/Ports/Interfaces/IPolicyPort.cs
@@ -0,0 +1,19 @@
+﻿using Orbital.Mock.Server.Models;
+using Orbital.Mock.Server.Models.Interfaces;
+using System.Collections.Generic;
+
+namespace Orbital.Mock.Server.Pipelines.Ports.Interfaces
+{
+    public interface IPolicyPort
+    {
+        /**
+         * The type of the policy
+         */
+        PolicyType Type { get; set; }
+
+        /**
+         * The list of policies to be used
+         */
+        IEnumerable<Policy> Policies { get; set; }
+    }
+}
