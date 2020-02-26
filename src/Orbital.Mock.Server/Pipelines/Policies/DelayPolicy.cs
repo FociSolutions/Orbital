0854e40ac7dc8507b17820662c7093e6b5b9e02e
Alex Yorke <alex.yorke@focisolutions.com>
2020-02-26 14:58:59 -0500

    Create DelayPolicy class

---
+++ src/Orbital.Mock.Server/Pipelines/Policies/DelayPolicy.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace Orbital.Mock.Server.Pipelines.Policies
+{
+    public static class DelayPolicy
+    {
+        public static bool Execute(int seconds)
+        {
+            int milliseconds = seconds * 1000;
+            Thread.Sleep(milliseconds);
+            return true;
+        }
+    }
+}
