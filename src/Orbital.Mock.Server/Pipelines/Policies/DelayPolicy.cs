d8233b9956fce4091b4ac83e6c65795997d2c73c
Alex Yorke <alex.yorke@focisolutions.com>
2020-02-27 12:01:30 -0500

    Use milliseconds instead of seconds for delaying request

--- src/Orbital.Mock.Server/Pipelines/Policies/DelayPolicy.cs
+++ src/Orbital.Mock.Server/Pipelines/Policies/DelayPolicy.cs
@@ -4,9 +4,11 @@ namespace Orbital.Mock.Server.Pipelines.Policies
 {
     public static class DelayPolicy
     {
-        public static bool Execute(int seconds)
+        /**
+         * Delays the request by the specified amount of milliseconds
+         */
+        public static bool Execute(int milliseconds)
         {
-            int milliseconds = seconds * 1000;
             Thread.Sleep(milliseconds);
             return true;
         }
