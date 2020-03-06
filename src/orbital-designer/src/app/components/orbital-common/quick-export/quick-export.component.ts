d17989c3808e49924aa78babc09d5cff5955c36c
Jonathan Cockrem <jonathan.cockrem@focisolutions.com>
2020-03-06 13:51:07 -0500

    Updated export to indicate server url

--- src/orbital-designer/src/app/components/orbital-common/quick-export/quick-export.component.ts
+++ src/orbital-designer/src/app/components/orbital-common/quick-export/quick-export.component.ts
@@ -39,7 +39,7 @@ export class QuickExportComponent implements OnInit {
         if (gotExported) {
           this.logger.debug('Mockdefinition has been exported: ', this.mockInService);
           this.logger.debug('To Url: ', this.urlInService);
-          this.exportStatusMessage = 'File successfully exported';
+          this.exportStatusMessage = 'File successfully exported to ' + this.urlInService;
           this.urlToNavigateTo = url;
         } else {
           this.exportStatusMessage = 'File could not be exported because of an error';
