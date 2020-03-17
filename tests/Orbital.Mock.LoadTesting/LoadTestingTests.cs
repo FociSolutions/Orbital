8bdbc9c3854b6097cc1098864ffb9d4600036dbc
Alex Yorke <alex.yorke@focisolutions.com>
2020-03-17 12:10:03 -0400

    Add TestsFixture class

--- tests/Orbital.Mock.LoadTesting/LoadTestingTests.cs
+++ tests/Orbital.Mock.LoadTesting/LoadTestingTests.cs
@@ -1,8 +1,9 @@
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Net;
+using System.Linq;
 using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
@@ -12,91 +13,84 @@ using Xunit;
 
 namespace Orbital.Mock.LoadTesting
 {
-    public class LoadTestingTests
+    public sealed class TestsFixture : IDisposable
     {
-        /// <summary>
-        /// Only one server should be allowed to run at once
-        /// </summary>
-        public static object serverRunning = new object();
-        private static readonly HttpClient client = new HttpClient();
         public static readonly int ORBITAL_PORT = 5001;
         public static readonly string ENDPOINT_PATH = "/api/v1/OrbitalAdmin";
         public static readonly string ORBITAL_URL = $"https://localhost:{ORBITAL_PORT}{ENDPOINT_PATH}";
-
-        [Fact]
-        public async void SendParallelRequestsTest()
+        public static Process OrbitalServerProcess;
+        public TestsFixture()
         {
-            Process p = StartServer();
-            IsPortAvailable();
-            var responses = new List<Task<HttpResponseMessage>>();
-
-            for (var i = 0; i < 100000; i++)
-            {
-                var response = client.GetAsync(ORBITAL_URL);
-                responses.Add(response);
-            }
-
-            // ensure that every response was successful
-            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
-
-            // stop server
-            TryStopProcess(p);
+            // Do "global" initialization here; Only called once.
         }
 
         /// <summary>
-        /// Sends the same mockdefinition multiple times to test the re-writing capability
-        /// of the database
+        /// Attempts to quit the server when an exception occurred and the server
+        /// was not terminated normally.
         /// </summary>
-        [Fact]
-        public async void SendSameMockdefinitionMultipleTimesTest()
+        public void Dispose()
         {
-            Process p = StartServer();
-            IsPortAvailable();
-            var responses = new List<Task<HttpResponseMessage>>();
-
-            for (var i = 0; i < 100; i++)
+            if (OrbitalServerProcess != null)
             {
-                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, ORBITAL_URL)
-                {
-                    Content = new StringContent(File.ReadAllText("./PetStoreBaseMockDefinition.json"), Encoding.UTF8, "application/json")
-                };
-
-                responses.Add(client.SendAsync(requestMessage));
+                TryStopProcess(OrbitalServerProcess);
             }
-
-            // ensure that every response was successful
-            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
-
-            // stop server
-            TryStopProcess(p);
         }
 
         /// <summary>
-        /// Sends the different mockdefinitions multiple times to test the memory capacity
-        /// of the server
+        /// Forceably stops a process
         /// </summary>
-        [Fact]
-        public async void MemoryMockdefinitionTest()
+        /// <param name="p">The process to stop</param>
+        private static void TryStopProcess(Process p)
         {
-            Process p = StartServer();
-            IsPortAvailable();
-            var responses = new List<Task<HttpResponseMessage>>();
-
-            for (var i = 0; i < 100; i++)
+            try
             {
-                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, ORBITAL_URL)
-                {
-                    Content = new StringContent(File.ReadAllText("./PetStoreBaseMockDefinition.json"), Encoding.UTF8, "application/json")
-                };
-
-                responses.Add(client.SendAsync(requestMessage));
+                p.Kill();
+                p.Close();
+                p.Dispose();
             }
+            catch (InvalidOperationException)
+            {
+                // do nothing because the process has already quit or does not exist
+            }
+        }
 
-            // ensure that every response was successful
-            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
+        public static void TryStopServer()
+        {
+            TryStopProcess(OrbitalServerProcess);
+        }
 
-            // stop server
-            TryStopProcess(p);
+        /// <summary>
+        /// Starts the server. Only one instance of the server can run at one time.
+        /// </summary>
+        /// <returns>Returns the process instance of this server.</returns>
+        public static void StartServer()
+        {
+            IsServerPortAvailable();
+            string workingDirectory = Environment.CurrentDirectory;
+            var projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.Parent;
+
+            string finalpath = $"{projectDirectory}\\src\\Orbital.Mock.Server\\bin\\{ReleaseType}\\netcoreapp2.2\\win-x64\\Orbital.Mock.Server.exe";
+            var p = new Process();
+
+            p.StartInfo.FileName = finalpath;
+
+            p.Start();
+            OrbitalServerProcess = p;
+        }
+
+        /// <summary>
+        /// Gets the current release type (e.g. Release/Debug) of the current configuration
+        /// </summary>
+        private static string ReleaseType
+        {
+            get
+            {
+#if DEBUG
+                return "Debug";
+#else
+            return "Release";
+#endif
+            }
         }
 
         /// <summary>
@@ -106,7 +100,7 @@ namespace Orbital.Mock.LoadTesting
         /// still be running. Other processes may start, but the old version of the server will take priority
         /// if it is still running or a local server could be running by accident.
         /// </summary>
-        public static void IsPortAvailable()
+        private static void IsServerPortAvailable()
         {
             var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
             var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
@@ -130,57 +124,88 @@ namespace Orbital.Mock.LoadTesting
                 }
             }
         }
+    }
 
+    public class LoadTestingTests : IClassFixture<TestsFixture>
+    {
         /// <summary>
-        /// Forceably stops a process
+        /// Only one server should be allowed to run at once
         /// </summary>
-        /// <param name="p">The process to stop</param>
-        private static void TryStopProcess(Process p)
+
+        private static readonly HttpClient client = new HttpClient();
+
+        [Fact]
+        public void SendParallelRequestsTest()
         {
-            try
+            TestsFixture.StartServer();
+            var responses = new List<Task<HttpResponseMessage>>();
+
+            for (var i = 0; i < 10000; i++)
             {
-                p.Kill();
+                var response = client.GetAsync(TestsFixture.ORBITAL_URL);
+                responses.Add(response);
             }
-            catch (InvalidOperationException)
-            {
-                // do nothing because the process has already quit or does not exist
-            }
+
+            // ensure that every response was successful
+            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
+
+            TestsFixture.TryStopServer();
         }
 
         /// <summary>
-        /// Starts the server. Only one instance of the server can run at one time.
+        /// Sends the same mockdefinition multiple times to test the re-writing capability
+        /// of the database
         /// </summary>
-        /// <returns>Returns the process instance of this server.</returns>
-        private static Process StartServer()
+        [Fact]
+        public void SendSameMockdefinitionMultipleTimesTest()
         {
-            lock (serverRunning)
+            TestsFixture.StartServer();
+            var responses = new List<Task<HttpResponseMessage>>();
+
+            for (var i = 0; i < 1000; i++)
             {
-                string workingDirectory = Environment.CurrentDirectory;
-                var projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.Parent;
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, TestsFixture.ORBITAL_URL)
+                {
+                    Content = new StringContent(File.ReadAllText("./PetStoreBaseMockDefinition.json"), Encoding.UTF8, "application/json")
+                };
 
-                string finalpath = $"{projectDirectory}\\src\\Orbital.Mock.Server\\bin\\{ReleaseType}\\netcoreapp2.2\\win-x64\\Orbital.Mock.Server.exe";
-                var p = new Process();
-
-                p.StartInfo.FileName = finalpath;
-                
-                p.Start();
-                return p;
+                responses.Add(client.SendAsync(requestMessage));
             }
+
+            // ensure that every response was successful
+            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
+
+            TestsFixture.TryStopServer();
         }
 
         /// <summary>
-        /// Gets the current release type (e.g. Release/Debug) of the current configuration
+        /// Sends the different mockdefinitions multiple times to test the memory capacity
+        /// of the server
         /// </summary>
-        public static string ReleaseType
+        [Fact]
+        public void MemoryMockdefinitionTest()
         {
-            get
+            TestsFixture.StartServer();
+            var requests = new List<HttpRequestMessage>();
+            var jsonMockdefinition = JObject.Parse(File.ReadAllText("./PetStoreBaseMockDefinition.json"));
+            for (var i = 0; i < 10000; i++)
             {
-#if DEBUG
-                return "Debug";
-#else
-            return "Release";
-#endif
+                jsonMockdefinition["metadata"]["title"] = i.ToString();
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, TestsFixture.ORBITAL_URL)
+                {
+                    Content = new StringContent(jsonMockdefinition.ToString(), Encoding.UTF8, "application/json")
+                };
+
+                requests.Add(requestMessage);
             }
+
+            // send them all at once because JSON parsing and writing is slow in the main loop
+            var responses = requests.Select(request => client.SendAsync(request)).ToList();
+
+            // ensure that every response was successful
+            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
+
+            TestsFixture.TryStopServer();
         }
     }
 }
