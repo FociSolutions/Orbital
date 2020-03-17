0d20df44f01e5aba69735331f68d0c6f1882f137
Alex Yorke <alex.yorke@focisolutions.com>
2020-03-17 11:15:20 -0400

    Add two load tests

--- tests/Orbital.Mock.LoadTesting/LoadTestingTests.cs
+++ tests/Orbital.Mock.LoadTesting/LoadTestingTests.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -11,31 +17,138 @@ namespace Orbital.Mock.LoadTesting
         /// <summary>
         /// Only one server should be allowed to run at once
         /// </summary>
-        public static object serverRunning;
+        public static object serverRunning = new object();
+        private static readonly HttpClient client = new HttpClient();
+        public static readonly int ORBITAL_PORT = 5001;
+        public static readonly string ENDPOINT_PATH = "/api/v1/OrbitalAdmin";
+        public static readonly string ORBITAL_URL = $"https://localhost:{ORBITAL_PORT}{ENDPOINT_PATH}";
 
         [Fact]
-        public async void SampleTestForFifteenSeconds()
+        public async void SendParallelRequestsTest()
         {
             Process p = StartServer();
+            IsPortAvailable();
+            var responses = new List<Task<HttpResponseMessage>>();
 
-            // wait 15 seconds to stop server
-            await Task.Delay((int)TimeSpan.FromSeconds(15).TotalMilliseconds);
+            for (var i = 0; i < 100000; i++)
+            {
+                var response = client.GetAsync(ORBITAL_URL);
+                responses.Add(response);
+            }
+
+            // ensure that every response was successful
+            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
 
             // stop server
-            StopProcess(p);
+            TryStopProcess(p);
+        }
+
+        /// <summary>
+        /// Sends the same mockdefinition multiple times to test the re-writing capability
+        /// of the database
+        /// </summary>
+        [Fact]
+        public async void SendSameMockdefinitionMultipleTimesTest()
+        {
+            Process p = StartServer();
+            IsPortAvailable();
+            var responses = new List<Task<HttpResponseMessage>>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, ORBITAL_URL)
+                {
+                    Content = new StringContent(File.ReadAllText("./PetStoreBaseMockDefinition.json"), Encoding.UTF8, "application/json")
+                };
+
+                responses.Add(client.SendAsync(requestMessage));
+            }
+
+            // ensure that every response was successful
+            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
+
+            // stop server
+            TryStopProcess(p);
+        }
+
+        /// <summary>
+        /// Sends the different mockdefinitions multiple times to test the memory capacity
+        /// of the server
+        /// </summary>
+        [Fact]
+        public async void MemoryMockdefinitionTest()
+        {
+            Process p = StartServer();
+            IsPortAvailable();
+            var responses = new List<Task<HttpResponseMessage>>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, ORBITAL_URL)
+                {
+                    Content = new StringContent(File.ReadAllText("./PetStoreBaseMockDefinition.json"), Encoding.UTF8, "application/json")
+                };
+
+                responses.Add(client.SendAsync(requestMessage));
+            }
+
+            // ensure that every response was successful
+            responses.ForEach(response => response.Result.EnsureSuccessStatusCode());
+
+            // stop server
+            TryStopProcess(p);
+        }
+
+        /// <summary>
+        /// Checks if a port is available by simulating results from netstat -aon | findstr 5001
+        /// It will not run taskkill /PID pid on the tasks; it will just throw an exception.
+        /// This is required because if the tests crash, it will not quit the process and the server will
+        /// still be running. Other processes may start, but the old version of the server will take priority
+        /// if it is still running or a local server could be running by accident.
+        /// </summary>
+        public static void IsPortAvailable()
+        {
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+            var tcpListenerInfoArray = ipGlobalProperties.GetActiveTcpListeners();
+
+            foreach (TcpConnectionInformation tcpi in tcpConnInfoArray)
+            {
+                if (tcpi.LocalEndPoint.Port == ORBITAL_PORT)
+                {
+                    // address in use error code WSAEADDRINUSE
+                    throw new SocketException(10048);
+                }
+            }
+
+            foreach (var tlpi in tcpListenerInfoArray)
+            {
+                if (tlpi.Port == ORBITAL_PORT)
+                {
+                    // address in use error code WSAEADDRINUSE
+                    throw new SocketException(10048);
+                }
+            }
         }
 
         /// <summary>
         /// Forceably stops a process
         /// </summary>
         /// <param name="p">The process to stop</param>
-        private static void StopProcess(Process p)
+        private static void TryStopProcess(Process p)
         {
-            p.Kill();
+            try
+            {
+                p.Kill();
+            }
+            catch (InvalidOperationException)
+            {
+                // do nothing because the process has already quit or does not exist
+            }
         }
 
         /// <summary>
-        /// Starts the server
+        /// Starts the server. Only one instance of the server can run at one time.
         /// </summary>
         /// <returns>Returns the process instance of this server.</returns>
         private static Process StartServer()
