a1a10ca6abf1151d004fe30dfa5f95be77735bbe
Alex Yorke <alex.yorke@focisolutions.com>
2020-03-23 14:02:28 -0400

    Fix some Mockdefinition tests

--- src/orbital-designer/src/app/services/mock-definition/mock-definition.service.spec.ts
+++ src/orbital-designer/src/app/services/mock-definition/mock-definition.service.spec.ts
@@ -13,9 +13,12 @@ import { BodyRule } from 'src/app/models/mock-definition/scenario/body-rule.mode
 import validOpenApiTest from '../../../test-files/valid-openapi-spec';
 import * as yaml from 'js-yaml';
 import { OpenAPIV2 } from 'openapi-types';
+import * as _ from 'lodash';
 
 describe('MockDefinitionService', () => {
   let store: DesignerStore;
+  let service: MockDefinitionService;
+  let _validMockDefinition = _.cloneDeep(validMockDefinition);
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [LoggerTestingModule],
@@ -24,16 +27,15 @@ describe('MockDefinitionService', () => {
   );
   beforeEach(() => {
     store = TestBed.get(DesignerStore);
-    store.mockDefinition = validMockDefinition as MockDefinition;
+    service = TestBed.get(MockDefinitionService);
+    store.mockDefinition = _validMockDefinition;
   });
 
   it('should be created', () => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
     expect(service).toBeTruthy();
   });
 
-  it('should clone a scenario from the store such that no name conflicts will be encountered', () => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
+  it('should clone a scenario from the store such that no name conflicts will be encountered', done => {
     const scenarios = [];
     for (let i = 0; i < 10; i++) {
       const mockverb = VerbType.GET;
@@ -68,12 +70,12 @@ describe('MockDefinitionService', () => {
     service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
       next: () => {
         expect(store.state.mockDefinition.scenarios.find(x => x.metadata.title.indexOf('-copy') !== -1)).toBeTruthy();
+        done();
       }
     });
   });
 
-  it('should clone a scenario from the store such that name conflicts will be encountered and will auto-rename', () => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
+  it('should clone a scenario from the store such that name conflicts will be encountered and will auto-rename', done => {
     const scenarios = [];
     for (let i = 0; i < 10; i++) {
       const mockverb = VerbType.GET;
@@ -106,23 +108,26 @@ describe('MockDefinitionService', () => {
 
     const scenarioToClone = JSON.parse(JSON.stringify(scenarios[0]));
 
-    service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe();
-
     service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
-      next: () => {
-        expect(store.state.mockDefinition.scenarios.find(x => x.metadata.title.indexOf('-copy 2') !== -1)).toBeTruthy();
+      complete: () => {
+        expect(store.state.mockDefinition.scenarios.filter(x => x.metadata.title.endsWith('-copy')).length).toBe(1);
+        service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
+          complete: () => {
+            expect(store.state.mockDefinition.scenarios.filter(x => x.metadata.title.endsWith('-copy 2')).length).toBe(1);
+            service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
+              complete: () => {
+                expect(store.state.mockDefinition.scenarios.filter(x => x.metadata.title.endsWith('-copy 3')).length).toBe(1);
+                done();
+              }
+            });
+          }
+        });
       }
-    });
-    service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
-      next: () => {
-        expect(store.state.mockDefinition.scenarios.find(x => x.metadata.title.indexOf('-copy 3') !== -1)).toBeTruthy();
-      }
-    });
+    });  
   });
 
-  it('should not clone a scenario from the store if the cloned scenario is invalid', () => {
+  it('should not clone a scenario from the store if the cloned scenario is invalid', done => {
     const scenarios = [];
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
     for (let i = 0; i < 10; i++) {
       const mockverb = VerbType.GET;
       const path = '/test';
@@ -155,16 +160,17 @@ describe('MockDefinitionService', () => {
     const scenarioLengthComponentExpected = store.state.mockDefinition.scenarios.length;
     const scenarioToClone = null;
     service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
-      error: () => {
+      next: (hasCloned) => {
         const scenarioLengthComponentActual = store.state.mockDefinition.scenarios.length;
         expect(scenarioLengthComponentActual).toEqual(scenarioLengthComponentExpected);
+        expect(hasCloned).toBe(false);
+        done();
       }
     });
   });
 
-  it('should clone a scenario and ensure that the title and id are different', () => {
+  it('should clone a scenario and ensure that the title and id are different', done => {
     const scenarios = [];
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
     for (let i = 0; i < 10; i++) {
       const mockverb = VerbType.GET;
       const path = '/test';
@@ -195,18 +201,18 @@ describe('MockDefinitionService', () => {
     store.state.mockDefinition.scenarios = scenarios;
     const scenarioToClone = scenarios[0];
     service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
-      next: () => {
-        expect(store.state.mockDefinition.scenarios[0].id).not.toEqual(store.mockDefinition.scenarios[1].id);
+      complete: () => {
+        expect(store.state.mockDefinition.scenarios[0].id).not.toEqual(store.state.mockDefinition.scenarios[1].id);
         expect(store.state.mockDefinition.scenarios[0].metadata.title).not.toEqual(
-          store.mockDefinition.scenarios[1].metadata.title
+          store.state.mockDefinition.scenarios[1].metadata.title
         );
+        done();
       }
     });
   });
 
-  it('should clone a scenario and ensure that there exists another scenario with the same contents, except for title and id', () => {
+  it('should clone a scenario and ensure that there exists another scenario with the same contents, except for title and id', done => {
     const scenarios = [];
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
 
     for (let i = 0; i < 3; i++) {
       const mockverb = VerbType.GET;
@@ -240,36 +246,35 @@ describe('MockDefinitionService', () => {
     const scenarioToClone = scenarios[0];
     service.cloneScenario(validMockDefinition.metadata.title, scenarioToClone).subscribe({
       next: n => {
-        console.log(n);
+        const componentScenarioClonee = store.state.mockDefinition.scenarios[0];
+
+        // ensure that there are two results after the cloning operation
+        const expectedResults = store.state.mockDefinition.scenarios.filter(
+          aScenario =>
+            aScenario.id !== componentScenarioClonee.id &&
+            aScenario.metadata.title !== componentScenarioClonee.metadata.title &&
+            aScenario.path === componentScenarioClonee.path &&
+            JSON.stringify(aScenario.requestMatchRules.bodyRules) ===
+              JSON.stringify(componentScenarioClonee.requestMatchRules.bodyRules) &&
+            JSON.stringify(aScenario.requestMatchRules.headerRules) ===
+              JSON.stringify(componentScenarioClonee.requestMatchRules.headerRules) &&
+            JSON.stringify(aScenario.requestMatchRules.queryRules) ===
+              JSON.stringify(componentScenarioClonee.requestMatchRules.queryRules) &&
+            aScenario.response.body === componentScenarioClonee.response.body &&
+            JSON.stringify(aScenario.response.headers) === JSON.stringify(componentScenarioClonee.response.headers) &&
+            aScenario.response.status === componentScenarioClonee.response.status &&
+            JSON.stringify(aScenario.verb) === JSON.stringify(componentScenarioClonee.verb) &&
+            aScenario.metadata.description === componentScenarioClonee.metadata.description
+        ).length;
+    
+        expect(expectedResults).toEqual(1);
+        done();
       }
     });
-    const componentScenarioClonee = store.state.mockDefinition.scenarios[0];
-
-    // ensure that there are two results after the cloning operation
-    const expectedResults = store.state.mockDefinition.scenarios.filter(
-      aScenario =>
-        aScenario.id !== componentScenarioClonee.id &&
-        aScenario.metadata.title !== componentScenarioClonee.metadata.title &&
-        aScenario.path === componentScenarioClonee.path &&
-        JSON.stringify(aScenario.requestMatchRules.bodyRules) ===
-          JSON.stringify(componentScenarioClonee.requestMatchRules.bodyRules) &&
-        JSON.stringify(aScenario.requestMatchRules.headerRules) ===
-          JSON.stringify(componentScenarioClonee.requestMatchRules.headerRules) &&
-        JSON.stringify(aScenario.requestMatchRules.queryRules) ===
-          JSON.stringify(componentScenarioClonee.requestMatchRules.queryRules) &&
-        aScenario.response.body === componentScenarioClonee.response.body &&
-        JSON.stringify(aScenario.response.headers) === JSON.stringify(componentScenarioClonee.response.headers) &&
-        aScenario.response.status === componentScenarioClonee.response.status &&
-        JSON.stringify(aScenario.verb) === JSON.stringify(componentScenarioClonee.verb) &&
-        aScenario.metadata.description === componentScenarioClonee.metadata.description
-    ).length;
-
-    expect(expectedResults).toEqual(1);
   });
 
   it('failed because content is not yaml', done => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
-
+    // add invalid Mockdefinition to trigger syntax error
     service.AddMockDefinitionToStore('%').subscribe({
       error: err => {
         expect(err).toBeTruthy();
@@ -279,8 +284,6 @@ describe('MockDefinitionService', () => {
   });
 
   it('succeed because content is valid yaml', done => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
-
     service.AddMockDefinitionToStore(validMockDefinitionFile).subscribe({
       next: t => {
         expect(t).toBeTruthy();
@@ -290,8 +293,6 @@ describe('MockDefinitionService', () => {
   });
 
   it('validation fails because content is not yaml', done => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
-
     service.validateMockDefinition('%').subscribe({
       error: err => {
         expect(err).toBeTruthy();
@@ -301,8 +302,6 @@ describe('MockDefinitionService', () => {
   });
 
   it('validation succeeds because content is valid yaml', done => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
-
     service.validateMockDefinition(validMockDefinitionFile).subscribe({
       next: t => {
         expect(t).toBeTruthy();
@@ -312,7 +311,6 @@ describe('MockDefinitionService', () => {
   });
 
   it('should generate default scenarios for imported open api endpoints', () => {
-    const service: MockDefinitionService = TestBed.get(MockDefinitionService);
     const content = yaml.safeLoad(validOpenApiTest) as OpenAPIV2.Document;
     const scenarios = service.getDefaultScenarios(content.paths);
     expect(scenarios.length).toBe(2);
