4c21c071d433fc97bdba9df2595cbbb3ea9ee28b
Maria reyes <maria.reyes@focisolutions.com>
2020-03-25 15:41:48 -0400

    add DatabaseFunctions class

--- src/Orbital.Mock.Server/Functions/BuiltinOrbitalFunctions.cs
+++ src/Orbital.Mock.Server/Functions/BuiltinOrbitalFunctions.cs
@@ -9,7 +9,7 @@ namespace Orbital.Mock.Server.Functions
         /// </summary>
         internal static readonly ScriptObject Default = new DefaultBuiltins();
 
-        public BuiltinOrbitalFunctions() : base(2)
+        public BuiltinOrbitalFunctions() : base(3)
         {
             ((ScriptObject)Default.Clone(true)).CopyTo(this);
         }
@@ -19,10 +19,11 @@ namespace Orbital.Mock.Server.Functions
         /// </summary>
         private class DefaultBuiltins : ScriptObject
         {
-            public DefaultBuiltins() : base(2, false)
+            public DefaultBuiltins() : base(3, false)
             {
                 SetValue("name", new NameFunctions(), true);
                 SetValue("address", new AddressFunctions(), true);
+                SetValue("database", new DatabaseFunctions(), true);
             }
         }
     }
