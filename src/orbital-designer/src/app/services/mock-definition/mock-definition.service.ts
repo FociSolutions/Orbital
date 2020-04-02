d00d302ec388e1a7d476404004baadc665f3523e
Alex Yorke <alex.yorke@focisolutions.com>
2020-04-02 09:42:57 -0400

    Change default type in generateNewScenario to custom

--- src/orbital-designer/src/app/services/mock-definition/mock-definition.service.ts
+++ src/orbital-designer/src/app/services/mock-definition/mock-definition.service.ts
@@ -200,7 +200,7 @@ export class MockDefinitionService {
         headers: {},
         body: '"default response for ' + path + '"',
         status: 200,
-        type: ResponseType.NONE
+        type: ResponseType.CUSTOM
       },
       requestMatchRules: {
         headerRules: [],
