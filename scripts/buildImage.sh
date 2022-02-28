62ec5d2329064ca6a4cd34fdd374ecc2523f0bde
glitch452 <894128+glitch452@users.noreply.github.com>
2022-02-28 13:51:53 -0500

    Created azure-pipelines-master pipeline and associated templates

--- scripts/buildImage.sh
+++ scripts/buildImage.sh
@@ -1,17 +1,17 @@
 #/bin/bash
-if [ "$RELEASE_ENVIRONMENTNAME" == "Dev" ] 
-then
+
+if [ "$RELEASE_ENVIRONMENTNAME" == "Dev" ]; then
     echo "Running in Development environment"
-elif [ "$RELEASE_ENVIRONMENTNAME" == "Prod" ] 
-then
+elif [ "$RELEASE_ENVIRONMENTNAME" == "Prod" ]; then
     echo "Running in Production environment"
-elif [ "$RELEASE_ENVIRONMENTNAME" == "UAT" ] 
-then
+elif [ "$RELEASE_ENVIRONMENTNAME" == "UAT" ]; then
     echo "Running in UAT environment"
 else
     echo "Running in Testing environment"
 fi
+
 echo "Build new Orbital Mock image"
-az acr build -t $IMAGENAMEMOCK:$1 -r $REGISTRY $SYSTEM_DEFAULTWORKINGDIRECTORY/_Orbital_CI/drop
+az acr build -t "$IMAGENAMEMOCK:$1" -r "$REGISTRY" "$SYSTEM_DEFAULTWORKINGDIRECTORY/_Orbital_CI/drop"
+
 echo "Build new Orbital Mock Designer image"
-az acr build -t $IMAGENAMEDESIGNER:$1 -r $REGISTRY $SYSTEM_DEFAULTWORKINGDIRECTORY/_Orbital_Designer_CI/drop
+az acr build -t "$IMAGENAMEDESIGNER:$1" -r "$REGISTRY" "$SYSTEM_DEFAULTWORKINGDIRECTORY/_Orbital_Designer_CI/drop"
