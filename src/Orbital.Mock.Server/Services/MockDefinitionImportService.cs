90ddf7aa2df26f7ec522efd08e580262d5ee2825
glitch452 <894128+glitch452@users.noreply.github.com>
2022-03-09 10:26:27 -0500

    Cleanup

--- src/Orbital.Mock.Server/Services/MockDefinitionImportService.cs
+++ src/Orbital.Mock.Server/Services/MockDefinitionImportService.cs
@@ -10,27 +10,52 @@ using Serilog;
 
 namespace Orbital.Mock.Server.Services
 {
+    /// <summary>
+    /// A utility class used to manage the importing and loading of MockDefinitions from various sources
+    /// </summary>
     public class MockDefinitionImportService : IMockDefinitionImportService
     {
         readonly IMemoryCache cache;
-        const string MockDefFile = "./pet_store_tests.json";
+        const string MockDefFile = "./mock_definition.json";
 
         public MockDefinitionImportService(IMemoryCache cache)
         {
             this.cache = cache;
         }
 
+        /// <summary>
+        /// Imports MockDefinitions from all supported sources and loads them into the MemoryCache.
+        /// </summary>
         public void ImportAllIntoMemoryCache()
         {
-            ImportFromDisk();
+            ImportFromFile(MockDefFile);
         }
 
-        void ImportFromDisk()
+        /// <summary>
+        /// Loads a MockDefinition from a file into the MemoryCache
+        /// </summary>
+        void ImportFromFile(string fileName)
         {
-            var mockDefinition = MockDefinition.CreateFromFile(MockDefFile);
+            if (System.IO.File.Exists(fileName))
+            {
+                var mockDefinition = MockDefinition.CreateFromFile(fileName);
 
-            Log.Information("MockDefinitionImportService: Imported Mock Definition from Disk, {mockDefinition}", mockDefinition.Metadata.Title);
+                Log.Information("MockDefinitionImportService: Imported Mock Definition from a File, {Title}", mockDefinition.Metadata.Title);
 
+                AddMockDefToMemoryCache(mockDefinition);
+            }
+            else
+            {
+                Log.Warning("MockDefinitionImportService: Unable to import Mock Definition, file does not exist: {FileName}", fileName);
+            }
+        }
+
+        /// <summary>
+        /// Loads the given MockDefinition into the memory cache
+        /// </summary>
+        /// <param name="mockDefinition"></param>
+        void AddMockDefToMemoryCache(MockDefinition mockDefinition)
+        {
             cache.Set(mockDefinition.Metadata.Title, mockDefinition);
             var keysCollection = this.cache.GetOrCreate(Constants.MOCK_IDS_CACHE_KEY, cacheEntry => { return new List<string>(); });
 
