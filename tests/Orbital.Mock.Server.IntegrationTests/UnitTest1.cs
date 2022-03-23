b1e1b73c3b9e18c8a6996bce141a4f7351e74126
glitch452 <894128+glitch452@users.noreply.github.com>
2022-03-23 15:29:59 -0400

    Created fresh integration tests project

---
+++ tests/Orbital.Mock.Server.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Orbital.Mock.Server.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
