aa0b18d329b916eb2a46c1da2e2993cd3bab32b8
Max Svetlakov <maksim.svetlakov@focisoulutions.com>
2022-03-24 15:38:26 -0400

    Missing files added

---
+++ tests/Orbital.Mock.Server.Integration.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System;
+
+namespace Orbital.Mock.Server.Integration.Tests
+{
+    #region snippet1
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                
+            });
+        }
+    }
+    #endregion
+}
