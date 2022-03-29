a039d116d6419206f3728a5fdd659b09763a9949
Emma Maddock <emma.maddock@focisolutions.com>
2022-03-29 13:27:57 -0400

    actually adding the changed files lol

--- src/Orbital.Mock.Server/HealthChecks/ServerHealthCheck.cs
+++ src/Orbital.Mock.Server/HealthChecks/ServerHealthCheck.cs
@@ -3,12 +3,28 @@ using System.Threading.Tasks;
 
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
+using Orbital.Mock.Definition.Response;
+using Orbital.Mock.Server.Pipelines.Models;
+using Orbital.Mock.Server.Pipelines.Models.Interfaces;
+
 namespace Orbital.Mock.Server.HealthChecks
 {
-    public class StartupHealthCheck : IHealthCheck
+    internal class ServerHealthCheck : IHealthCheck
     {
+        private volatile bool isPipelineRunning;
         private volatile bool isReady;
 
+        private readonly IPipeline<MessageProcessorInput, Task<MockResponse>> _pipeline;
+
+        public ServerHealthCheck(IPipeline<MessageProcessorInput, Task<MockResponse>> pipeline)
+        {
+            _pipeline = pipeline;
+            
+            isPipelineRunning = false;
+            isReady = false;
+        }
+
+        // returns true when the StartupBackgroundService has completed
         public bool StartupCompleted
         {
             get => isReady;
@@ -17,9 +33,18 @@ namespace Orbital.Mock.Server.HealthChecks
 
         public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
-            if (StartupCompleted) return Task.FromResult(HealthCheckResult.Healthy("StartupBackgroundService has completed configuration!"));
+            isPipelineRunning = _pipeline.GetPipelineStatus();
 
-            return Task.FromResult(HealthCheckResult.Unhealthy("StartupBackgroundService is still configuring underlying services!"));
+            if (!isPipelineRunning)
+                return Task.FromResult(HealthCheckResult.Unhealthy("The pipeline is not running yet!"));
+
+            if (!StartupCompleted)
+                return Task.FromResult(HealthCheckResult.Unhealthy("StartupBackgroundService is still configuring underlying services!"));
+
+            if (StartupCompleted && isPipelineRunning)
+                return Task.FromResult(HealthCheckResult.Healthy("Configuration is complete and the pipeline is running. The server is ready to take requests!"));
+
+            return Task.FromResult(HealthCheckResult.Unhealthy("An unexpected error occurred."));
         }
     }
 }
