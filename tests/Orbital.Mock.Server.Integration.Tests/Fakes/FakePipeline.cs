43dedca5b898a85163041af9cbceadb4c8d52d51
Emma Maddock <emma.madds@gmail.com>
2022-03-29 15:24:17 -0400

    establishing structure for tests/fake pipeline dependencies/references

---
+++ tests/Orbital.Mock.Server.Integration.Tests/Fakes/FakePipeline.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Orbital.Mock.Server.Pipelines.Models.Interfaces;
+
+namespace Orbital.Mock.Server.Integration.Tests.Fakes
+{
+    internal class FakePipeline : IPipeline
+    {
+        public FakePipeline()
+        {
+        }
+
+        public bool GetPipelineStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
