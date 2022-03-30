c14a2cd7577d569b842482e31e909032a5014d07
glitch452 <894128+glitch452@users.noreply.github.com>
2022-03-30 11:48:04 -0400

    Added get by id test for admin controller

---
+++ tests/Orbital.Mock.Server.Integration.Tests/Controllers/OrbitalAdminControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Orbital.Mock.Definition;
+
+namespace Orbital.Mock.Server.Integration.Tests
+{
+    public class OrbitalAdminControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public OrbitalAdminControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            var pathVarName = $"{Constants.ENV_PREFIX}{Constants.MOCK_DEF_IMPORT_SVC_SECTION_NAME}__PATH";
+            var mockDefPath = Path.Combine(".", "fixtures", "mock_definition_valid.json");
+            Environment.SetEnvironmentVariable(pathVarName, mockDefPath);
+
+            _client = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsLoadedMockDef()
+        {
+            #region Test Setup
+            var expected = "Pet Store Tests";
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{Constants.ADMIN_ENDPOINT_URL}/{expected}");
+            #endregion
+
+            var response = await _client.SendAsync(request);
+
+            Assert.True(response.IsSuccessStatusCode);
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var mockDef = MockDefinition.CreateFromJsonString(responseBody);
+
+            Assert.Equal(expected, mockDef.Metadata.Title);
+        }
+
+    }
+}
