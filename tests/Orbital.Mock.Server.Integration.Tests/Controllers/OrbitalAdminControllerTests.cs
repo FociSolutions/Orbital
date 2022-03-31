5dd6e4eac0017fab9dd6ba4fff031fee5aa9a449
glitch452 <894128+glitch452@users.noreply.github.com>
2022-03-31 12:46:01 -0400

    Added basic put mock def test

--- tests/Orbital.Mock.Server.Integration.Tests/Controllers/OrbitalAdminControllerTests.cs
+++ tests/Orbital.Mock.Server.Integration.Tests/Controllers/OrbitalAdminControllerTests.cs
@@ -6,18 +6,25 @@ using System.Net.Http;
 using System.Threading.Tasks;
 
 using Orbital.Mock.Definition;
+using Bogus;
+using System.Text;
 
 namespace Orbital.Mock.Server.Integration.Tests
 {
     public class OrbitalAdminControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
         private readonly HttpClient _client;
+        private readonly string mockDefPath;
+        private readonly Faker faker;
+
 
         public OrbitalAdminControllerTests(CustomWebApplicationFactory<Startup> factory)
         {
             var pathVarName = $"{Constants.ENV_PREFIX}{Constants.MOCK_DEF_IMPORT_SVC_SECTION_NAME}__PATH";
-            var mockDefPath = Path.Combine(".", "fixtures");
+            mockDefPath = Path.Combine(".", "fixtures");
             Environment.SetEnvironmentVariable(pathVarName, mockDefPath);
+
+            faker = new Faker();
 
             _client = factory.CreateDefaultClient();
         }
@@ -57,5 +64,30 @@ namespace Orbital.Mock.Server.Integration.Tests
 
             Assert.Equal(expected, mockDef.Metadata.Title);
         }
+
+        [Fact]
+        public async Task PutMockDef_UpdatesMockDef()
+        {
+            #region Test Setup
+            var mockDef = MockDefinition.CreateFromFile(mockDefPath);
+            var expected = faker.Lorem.Sentence();
+            mockDef.Metadata.Description = expected;
+
+            var requestContent = new StringContent(mockDef.ToJson(), Encoding.UTF8, "application/json");
+            var getRequest = new HttpRequestMessage(HttpMethod.Get, $"{Constants.ADMIN_ENDPOINT_URL}/{mockDef.Metadata.Title}");
+            #endregion
+
+            var putResponse = await _client.PutAsync(Constants.ADMIN_ENDPOINT_URL, requestContent);
+            var getResponse = await _client.SendAsync(getRequest);
+
+            Assert.True(putResponse.IsSuccessStatusCode);
+            Assert.True(getResponse.IsSuccessStatusCode);
+
+            var responseBody = await getResponse.Content.ReadAsStringAsync();
+            var responseMockDef = MockDefinition.CreateFromJsonString(responseBody);
+
+            Assert.Equal(expected, responseMockDef.Metadata.Description);
+        }
+
     }
 }
