48a2ab5513da631ab4807833696c0bf9cce4c470
Reuben Kogie <reuben.kogie@focisolutions.com>
2023-03-10 00:22:44 +0000

    Updated dialogue module - Build Successful

--- src/orbital-designer/src/app/shared/material.module.ts
+++ src/orbital-designer/src/app/shared/material.module.ts
@@ -17,7 +17,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatGridListModule } from '@angular/material/grid-list';
