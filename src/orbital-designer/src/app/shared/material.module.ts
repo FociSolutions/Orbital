37fb05e7c42e1031e1ab4aed55952876a2e3ff32
Reuben Kogie <reuben.kogie@focisolutions.com>
2023-03-13 17:43:14 +0000

    Updated table component

--- src/orbital-designer/src/app/shared/material.module.ts
+++ src/orbital-designer/src/app/shared/material.module.ts
@@ -35,7 +35,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
 import { MatSortModule } from '@angular/material/sort';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
