b2b8ee3833ac6bf9e37ff0aeeb696870db8fb2d1
Reuben Kogie <reuben.kogie@focisolutions.com>
2023-03-16 01:34:41 +0000

    Updated Chips module & fixed CSS

--- src/orbital-designer/src/app/shared/material.module.ts
+++ src/orbital-designer/src/app/shared/material.module.ts
@@ -13,7 +13,7 @@ import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/lega
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
